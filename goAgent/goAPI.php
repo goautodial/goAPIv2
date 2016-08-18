aa0593f2f3e2b6f9f700ac3dafc7263a2b42e0ea
demian <demian@goautodial.com>
2016-08-18 11:19:23 -0400

    Updates from WebRTC server

--- goAgent/goAPI.php
+++ goAgent/goAPI.php
@@ -114,7 +114,7 @@ $actions = implode('|', $fileList);
 if (isset($goAction) && $goAction != "") {
     if (preg_match("/$actions/", $goAction)) {
         $system = get_settings('system', $astDB);
-        $bcrypt = (isset($bcrypt)) ? $bcrypt : 0;
+        $bcrypt = (isset($bcrypt)) ? $bcrypt : 1;
         $err_message = "Login incorrect, please try again";
         $auth_message = user_authorization($astDB, $goUser, $goPass, '', 1, $bcrypt, 0);
         if ($auth_message == 'GOOD')
