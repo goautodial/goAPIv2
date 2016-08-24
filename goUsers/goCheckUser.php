3b61e1075abd6185febebbc77d65f53d472f15cd
demian <demian@goautodial.com>
2016-08-24 15:40:49 -0400

    Check duplicates in Users

--- goUsers/goCheckUser.php
+++ goUsers/goCheckUser.php
@@ -14,36 +14,36 @@
         $user = mysqli_real_escape_string($link, $_REQUEST['user']);
         $phone_login = mysqli_real_escape_string($link, $_REQUEST['phone_login']);
 
-        $response = array();
-        $validate = 0;
+        $validate1 = 0;
+        $validate2 = 0;
+        
+        // Phone Login Check optional when not null
+        if($phone_login != NULL){
+            $queryPhoneCheck = "SELECT user FROM vicidial_users WHERE phone_login = '$phone_login';";
+            $rsltvCheck2 = mysqli_query($link, $queryPhoneCheck);
+            $countCheckResult2 = mysqli_num_rows($rsltvCheck2);
+                
+                if($countCheckResult2 > 0) {
+                    $validate2 = $validate2 + 1;
+                    $apiresults = array("result" => "phone_login", "phone_login" => "There are 1 or more users with that Phone Login.");
+                }else{
+                    $validate2 = 0;
+                }
+        }
         
         // User Duplicate Check
         $queryUserCheck = "SELECT user FROM vicidial_users WHERE user = '$user';";
         $rsltvCheck1 = mysqli_query($link, $queryUserCheck);
         $countCheckResult1 = mysqli_num_rows($rsltvCheck1);
-        if($countCheckResult1 > 0) {
-            $validate = $validate + 1;
-            $response = array_push("user" => "There are 1 or more users with that User ID.");
-        }else{
-            $validate = $validate - 1;
-        }
         
-        // Phone Login Check optional when not null
-        if($phone_login != NULL){
-            $queryPhoneCheck = "SELECT user FROM vicidial_users WHERE phone_login = '$phone_login';";
-            $rsltvCheck2 = mysqli_query($link, $queryNameCheck);
-            $countCheckResult2 = mysqli_num_rows($rsltvCheck2);
-            if($countCheckResult2 > 0) {
-                $validate = $validate + 1;
-                $response = array_push("phone login" => "There are 1 or more users with that Phone Login.");
+            if($countCheckResult1 > 0) {
+                $validate1 = $validate1 + 1;
+                $apiresults = array("result" => "user", "user" => "There are 1 or more users with that User ID.");
             }else{
-                $validate = $validate - 1;
+                $validate1 = 0;
             }
-        }
         
-        if($validate <= 0){
+        if($validate1 == 0 && $validate2 == 0){
             $apiresults = array("result" => "success");
-        }else{
-            $apiresults = array_merge("result" => "fail", "data" => $response);
         }
 ?>
--- goUsers/goGetAllUserLists.php
+++ goUsers/goGetAllUserLists.php
@@ -2,13 +2,12 @@
     #######################################################
     #### Name: goGetAllUserLists.php 	               ####
     #### Description: API to get all User Lists        ####
-    #### Version: 0.9                                  ####
-    #### Copyright: GOAutoDial Ltd. (c) 2011-2015      ####
+    #### Version: 4.0                                  ####
+    #### Copyright: GOAutoDial Ltd. (c) 2011-2016      ####
     #### Written by: Jeremiah Sebastian V. Samatra     ####
     #### License: AGPLv2                               ####
     #######################################################
-    include_once ("goFunctions.php");
-
+    include_once ("../goFunctions.php");
 
     $groupId = go_get_groupid($goUser);
     
@@ -21,27 +20,51 @@
         $notAdminSQL = "AND user_group != 'ADMIN'";
     }
 
-#	$query = "SELECT user_id, user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $ul $notAdminSQL ORDER BY user ASC;";
-	$query = "SELECT user_id, user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $notAdminSQL ORDER BY user ASC;";
-   	$getLastCount = "SELECT user FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $notAdminSQL ORDER BY user ASC;";
-	
+	// getting agent count
+	$getLastCount = "SELECT user FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $notAdminSQL ORDER BY user ASC;";
 	$queryCount = mysqli_query($link, $getLastCount);
-	
 	$max = mysqli_num_rows($queryCount);
-	for($i=0; $i < $max; $i++){
-		$userRow = mysqli_fetch_array($queryCount);
-		if(preg_match("/^agent/i", $userRow['user'])){
-			$get_last = preg_replace("/^agent/i", "", $userRow['user']);
-			$last_num[] = intval($get_last);
+		
+		// condition
+		for($i=0; $i < $max; $i++){
+			$userRow = mysqli_fetch_array($queryCount);
+			if(preg_match("/^agent/i", $userRow['user'])){
+				$get_last = preg_replace("/^agent/i", "", $userRow['user']);
+				$last_num[] = intval($get_last);
+			}
 		}
-	}
-
-	$get_last = max($last_num);
-	$agent_num = $get_last + 1;
+		// return data
+		$get_last = max($last_num);
+		$agent_num = $get_last + 1;
+		
+	// getting phone login count
+	$getLastPhoneLogin = "SELECT phone_login FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' AND phone_login != '' $notAdminSQL ORDER BY phone_login DESC;";
+	$queryPhoneLoginCount = mysqli_query($link, $getLastPhoneLogin);
+	$max_phonelogins = mysqli_num_rows($queryPhoneLoginCount);
 	
+		// condition
+		while($get_last_phonelogin = mysqli_fetch_array($queryPhoneLoginCount)){
+			if(preg_match("/^Agent/i", $get_last_phonelogin['phone_login'])){
+				$get_last_count = preg_replace("/^Agent/i", "", $get_last_phonelogin['phone_login']);
+				$last_pl[] = intval($get_last_count);
+			}else{
+				$get_last_count = $get_last_phonelogin['phone_login'];
+				$last_pl[] = intval($get_last_count);
+			}
+		}
+		
+		// return data
+		$phonelogin_num = max($last_pl);
+		$phonelogin_num = $phonelogin_num + 1;
+	
+	
+	// getting all users
+	#	$query = "SELECT user_id, user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $ul $notAdminSQL ORDER BY user ASC;";
+	$query = "SELECT user_id, user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $notAdminSQL ORDER BY user ASC;";
 	$rsltv = mysqli_query($link, $query);
-        $countResult = mysqli_num_rows($rsltv);
-
+    $countResult = mysqli_num_rows($rsltv);
+		
+		// condition
         if($countResult > 0) {
         while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
                 $dataUserID[] = $fresults['user_id'];
@@ -49,8 +72,8 @@
                 $dataFullName[] = $fresults['full_name'];
                 $dataUserLevel[] = $fresults['user_level'];
                 $dataUserGroup[] = $fresults['user_group'];
-		$dataActive[]	= $fresults['active'];
-                $apiresults = array("result" => "success", "user_id" => $dataUserID,"user_group" => $dataUserGroup, "user" => $dataUser, "full_name" => $dataFullName, "user_level" => $dataUserLevel, "active" => $dataActive, "last_count" => $agent_num);
+				$dataActive[]	= $fresults['active'];
+                $apiresults = array("result" => "success", "user_id" => $dataUserID,"user_group" => $dataUserGroup, "user" => $dataUser, "full_name" => $dataFullName, "user_level" => $dataUserLevel, "active" => $dataActive, "last_count" => $agent_num, "last_phone_login" => $phonelogin_num);
         }
 	} else {
 		$apiresults = array("result" => "Error: No data to show.");
