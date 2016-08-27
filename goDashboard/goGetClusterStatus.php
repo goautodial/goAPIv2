4e3020268cbc6f79288094e56e45b00dcfd19384
Demian Biscocho <demian@goautodial.com>
2016-08-26 23:51:13 -0400

    Minor revisions.

--- goDashboard/goGetClusterStatus.php
+++ goDashboard/goGetClusterStatus.php
@@ -5,24 +5,27 @@
     #### Version: 0.9                               ####
     #### Copyright: GOAutoDial Inc. (c) 2011-2014   ####
     #### Written by: Jeremiah Sebastian Samatra     ####
-    ####             Demian Lizandro A. Biscocho    ####
+    ####            Demian Lizandro A. Biscocho     ####
     #### License: AGPLv2                            ####
     ####################################################
     
     include_once("../goFunctions.php");
+    
+    $query = "SELECT s.server_id, s.server_description, s.server_ip, s.active, s.sysload, s.channels_total, s.cpu_idle_percent, s.disk_usage, su.last_update as s_time,UNIX_TIMESTAMP(su.last_update) as u_time FROM servers s, server_updater su WHERE s.server_ip=su.server_ip";
 
-	    
-	$query = "SELECT server_id,server_description,server_ip,active,sysload,channels_total,cpu_idle_percent,disk_usage from servers order by server_id;";
-	$rsltv = mysqli_query($link,$query);
-	$countResult = mysqli_num_rows($rsltv);
+    $rsltv = mysqli_query($link,$query);
+    $countResult = mysqli_num_rows($rsltv);
 
-        if($countResult > 0) {
-            $data = array();
-                while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){               
-                    array_push($data, $fresults);
-                }
-                $apiresults = array("result" => "success", "data" => $data);
-        } 	
-	
+    if($countResult > 0){
+    
+        $data = array();
+        
+        while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){       
+            array_push($data, $fresults);
+        }
+        
+        $apiresults = array("result" => "success", "data" => $data);
+    } 
+
 
 ?>
