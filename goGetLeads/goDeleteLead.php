edcebede318a233cb4d90cd8c39c66df7db17634
demian <demian@goautodial.com>
2016-09-08 11:01:39 -0400

    Bug #3159 Fix: Condition on output success

--- goGetLeads/goDeleteLead.php
+++ goGetLeads/goDeleteLead.php
@@ -5,11 +5,12 @@
     #### Copyright: GOAutoDial Inc. (c) 2016           ####
     #### Written by: Alexander Abenoja _m/             ####
     #######################################################
-    include_once ("goFunctions.php");
+    include_once("../goFunctions.php");
 
     ### POST or GET Variables
         $lead_id = $_REQUEST['lead_id'];
-
+		$ip_address = $_REQUEST['hostname'];
+		
     ### Check user_id if its null or empty
         if($lead_id == null) {
                 $apiresults = array("result" => "Error: Set a value for Lead ID.");
@@ -29,19 +30,19 @@
 
                 $query = "DELETE FROM vicidial_list WHERE lead_id='$lead_id'";
                 $rsltv = mysqli_query($link, $query);
-         	$countResult = mysqli_num_rows($rsltv);
+				$countResult = mysqli_num_rows($rsltv);
 
-		if($countResult > 0){
-			 ### Admin logs
-                	$SQLdate = date("Y-m-d H:i:s");
-                	$queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','DELETE','Deleted Lead ID $lead_id','DELETE FROM vicidial_list WHERE lead_id=$lead_id');";
-                        $rsltvLog = mysqli_query($linkgo, $queryLog);
-
-                        $apiresults = array("result" => "success");
-		}else{
-			 $apiresults = array("result" => "Error: Lead ID does not exist.");
-
-		}
+				if($rsltv != false){
+					 ### Admin logs
+							$SQLdate = date("Y-m-d H:i:s");
+							$queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','DELETE','Deleted Lead ID $lead_id','DELETE FROM vicidial_list WHERE lead_id=$lead_id');";
+								$rsltvLog = mysqli_query($linkgo, $queryLog);
+		
+								$apiresults = array("result" => "success");
+				}else{
+					 $apiresults = array("result" => "Error: Lead ID does not exist.");
+		
+				}
 		
         }
 ?>
