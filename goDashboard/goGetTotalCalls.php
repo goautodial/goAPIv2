282954e736b2f7f937e47a21a15d4f3ca3ce38f2
Demian Biscocho <demian@goautodial.com>
2016-09-11 04:08:44 -0400

    Minor revisions.

--- goDashboard/goGetTotalCalls.php
+++ goDashboard/goGetTotalCalls.php
@@ -1,12 +1,13 @@
 <?php
-    ##############################################################
-    #### Name: goGetTotalCalls.php            	    	      ####
-    #### Description: API to get total calls		      ####
-    #### Version: 0.9                              	      ####
-    #### Copyright: GOAutoDial Inc. (c) 2011-2014  	      ####
-    #### Written by: Jeremiah Sebastian V. Samatra 	      ####
-    #### License: AGPLv2                           	      ####
-    ##############################################################
+    ########################################################
+    #### Name: goGetTotalCalls.php                      ####
+    #### Description: API to get total calls            ####
+    #### Version: 0.9                                   ####
+    #### Copyright: GOAutoDial Inc. (c) 2011-2016       ####
+    #### Written by: Jeremiah Sebastian V. Samatra      ####
+    ####             Demian Lizandro A. Biscocho        ####
+    #### License: AGPLv2                                ####
+    ########################################################
     
     include_once("../goFunctions.php");
     
