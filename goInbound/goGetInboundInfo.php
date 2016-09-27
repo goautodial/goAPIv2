ad89f5c20de817d8b5292138778582cb2e7eeb4d
demian <demian@goautodial.com>
2016-09-26 23:20:16 -0400

    Update on basic and advanced settings

--- goInbound/goGetInboundInfo.php
+++ goInbound/goGetInboundInfo.php
@@ -7,7 +7,7 @@
     #### Written by: Jerico James Milo                 ####
     #### License: AGPLv2                               ####
     #######################################################
-    include_once ("goFunctions.php");
+    include_once ("../goFunctions.php");
     
     ### POST or GET Variables
     $group_id = $_REQUEST['group_id'];
@@ -24,22 +24,15 @@
 			$ul = "WHERE group_id='$group_id' AND user_group='$groupId'";  
 		}
 
-   		$query = "SELECT group_id,group_name,queue_priority,active,call_time_id,group_color FROM vicidial_inbound_groups $ul ORDER BY group_id LIMIT 1;";
+   		$query = "SELECT group_id, group_name, group_color, active, web_form_address, next_agent_call, queue_priority, fronter_display, ingroup_script, drop_call_seconds, drop_action, drop_exten, drop_inbound_group, drop_callmenu, voicemail_ext, call_time_id, after_hours_action, after_hours_message_filename, after_hours_exten, after_hours_voicemail, get_call_launch, no_agent_no_queue, no_agent_action, no_agent_action_value, welcome_message_filename, play_welcome_message, moh_context, onhold_prompt_filename FROM vicidial_inbound_groups $ul ORDER BY group_id LIMIT 1;";
    		$rsltv = mysqli_query($link, $query);
 		$countResult = mysqli_num_rows($rsltv);
 
 		if($countResult > 0) {
-			while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
-        $dataGroupId[] =  $fresults['group_id'];
-        $dataGroupName[] =  $fresults['group_name'];
-        $dataQueuePriority[] =  $fresults['queue_priority'];
-        $dataActive[] =  $fresults['active'];
-        $dataCallTimeId[] =  $fresults['call_time_id'];
-		$dataGroupColor[] =  $fresults['group_color'];
+			$fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC);
 		
-    $apiresults = array( "result" => "success", "group_id" => $dataGroupId, "group_name" => $dataGroupName, "queue_priority" => $dataQueuePriority, "active" => $dataActive, "call_time_id" => $dataCallTimeId, "group_color" => $dataGroupColor);
+			$apiresults = array( "result" => "success", "data" => $fresults);
 
-			}
 		} else {
 			$apiresults = array("result" => "Error: Inbound doesn't exist.");
 		}
