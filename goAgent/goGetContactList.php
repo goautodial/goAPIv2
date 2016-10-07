6eaea9ab2c5dd86af3e86575eac8531175e24de8
demian <demian@goautodial.com>
2016-10-07 17:57:58 -0400

    Updates from WebRTC server

--- goAgent/goGetContactList.php
+++ goAgent/goGetContactList.php
@@ -35,7 +35,7 @@ foreach ($rslt as $val) {
 if (count($list_ids) > 0 ) {
     $astDB->where('vl.list_id', $list_ids, 'in');
     $astDB->join('vicidial_lists vls', 'vls.list_id=vl.list_id', 'left');
-    $rslt = $astDB->get('vicidial_list vl', $limit, 'lead_id,first_name,middle_initial,last_name,phone_number,last_local_call_time,campaign_id,status,comments');
+    $rslt = $astDB->get('vicidial_list vl', $limit, 'lead_id,first_name,middle_initial,last_name,phone_number,last_local_call_time,campaign_id,status,comments,phone_code');
 
     foreach ($rslt as $lead) {
         $leads[] = $lead;
