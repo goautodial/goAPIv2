09e162af5fad381de4e86f6e2e76a535e0363260
demianb <demian@goautodial.com>
2016-11-27 15:17:56 -0500

    Added TotalSalesYesterday and TotalSalesLastWeek.

--- goDashboard/goGetTotalSales.php
+++ goDashboard/goGetTotalSales.php
@@ -24,12 +24,24 @@
     $query_date =  date('Y-m-d');
     $status = "SALE";
     $date = "call_date BETWEEN '$query_date 00:00:00' AND '$query_date 23:59:59'";
+    $dateToday = "CURDATE()";
+    $dateYesterday = "SUBDATE(CURDATE(),1)";
+    $dateLastWeek = "between NOW() - INTERVAL DAYOFWEEK(NOW())+6 DAY AND NOW() - INTERVAL DAYOFWEEK(NOW())-1 DAY";
    
     $query = "select (select count(*) from vicidial_closer_log vcl,vicidial_agent_log val where vcl.uniqueid=val.uniqueid and val.status='$status' and $date $ul) + (select count(*) from vicidial_log vl,vicidial_agent_log val where vl.uniqueid=val.uniqueid and val.status='$status' and $date $ul) as TotalSales";
-	
+    $queryToday = "select (select count(*) from vicidial_closer_log vcl,vicidial_agent_log val where vcl.uniqueid=val.uniqueid and val.status='$status' and call_date='$dateToday' $ul) + (select count(*) from vicidial_log vl,vicidial_agent_log val where vl.uniqueid=val.uniqueid and val.status='$status' and call_date='$dateToday' $ul) as TotalSalesToday";
+    $queryYesterday = "select (select count(*) from vicidial_closer_log vcl,vicidial_agent_log val where vcl.uniqueid=val.uniqueid and val.status='$status' and call_date='$dateYesterday' $ul) + (select count(*) from vicidial_log vl,vicidial_agent_log val where vl.uniqueid=val.uniqueid and val.status='$status' and call_date='$dateYesterday' $ul) as TotalSalesYesterday";
+    $queryLastWeek = "select (select count(*) from vicidial_closer_log vcl,vicidial_agent_log val where vcl.uniqueid=val.uniqueid and val.status='$status' and call_date $dateLastWeek $ul) + (select count(*) from vicidial_log vl,vicidial_agent_log val where vl.uniqueid=val.uniqueid and val.status='$status' and call_date $dateLastWeek $ul) as TotalSalesLastWeek";
+    
     //	$query = "select (select count(*) from vicidial_closer_log vcl,vicidial_agent_log val where vcl.uniqueid=val.uniqueid ) + (select count(*) from vicidial_log vl,vicidial_agent_log val where vl.uniqueid=val.uniqueid ) as TotalSales;";
     //$drop_percentage = ( ($line->drops_today / $line->answers_today) * 100); 
     $rsltv = mysqli_query($link,$query);
+    $rsltvToday =  mysqli_query($link,$queryToday);
+    $rsltvYesterday =  mysqli_query($link,$queryYesterday);
+    $rsltvLastWeek =  mysqli_query($link,$queryLastWeek);
     $fresults = mysqli_fetch_assoc($rsltv);
-    $apiresults = array_merge( array( "result" => "success" ), $fresults );
+    $fresultsToday = mysqli_fetch_assoc($rsltvToday);    
+    $fresultsYesterday = mysqli_fetch_assoc($rsltvYesterday);
+    $fresultsLastWeek = mysqli_fetch_assoc($rsltvLastWeek);
+    $apiresults = array_merge( array( "result" => "success" ), $fresults, $fresultsToday, $fresultsYesterday, $fresultsLastWeek);
 ?>
