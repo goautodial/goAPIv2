a539c06fa5572c2250093fc3a6c3792522209b51
demian <demian@goautodial.com>
2016-12-02 16:27:04 -0500

    Updated Carrier Edit

--- goCarriers/goEditCarrier.php
+++ goCarriers/goEditCarrier.php
@@ -16,9 +16,10 @@
     $carrier_description = $_REQUEST['carrier_description'];
     $protocol = $_REQUEST['protocol'];
     $server_ip = $_REQUEST['server_ip'];
-  //  $registration_string = $_REQUEST['registration_string'];
-  //  $account_entry = $_REQUEST['account_entry'];
-  //  $global_string = $_REQUEST['global_string'];
+    $registration_string = $_REQUEST['registration_string'];
+    $account_entry = $_REQUEST['account_entry'];
+	$dialplan_entry = $_REQUEST['dialplan_entry'];
+    $globals_string = $_REQUEST['globals_string'];
     $active = strtoupper($_REQUEST['active']);
     $goUser = $_REQUEST['goUser'];
     $ip_address = $_REQUEST['hostname'];
@@ -34,13 +35,12 @@
         if($carrier_id == null) {
                 $apiresults = array("result" => "Error: Set a value for Carrier ID.");
         } else   {
-                if(!in_array($active,$defActive) && $active != null) {
-                        $apiresults = array("result" => "Error: Default value for active is Y or N only.");
-                } else {
-
-                if(!in_array($protocol,$defProtocol) && $protocol != null) {
-                        $apiresults = array("result" => "Error: Default value for protocol is SIP, Zap, IAX2 or EXTERNAL only");
-                } else {
+		if(!in_array($active,$defActive) && $active != null) {
+				$apiresults = array("result" => "Error: Default value for active is Y or N only.");
+		} else {
+		if(!in_array($protocol,$defProtocol) && $protocol != null) {
+				$apiresults = array("result" => "Error: Default value for protocol is SIP, Zap, IAX2 or EXTERNAL only");
+		} else {
         if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $registration_string)){
                 $apiresults = array("result" => "Error: Special characters found in registration_string");
         } else {
@@ -53,14 +53,6 @@
         if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $global_string)){
                 $apiresults = array("result" => "Error: Special characters found in global_string");
         } else {
-        if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $account_entry)){
-                $apiresults = array("result" => "Error: Special characters found in account_entry");
-        } else {
-        if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $server_ip)){
-                $apiresults = array("result" => "Error: Special characters found in server_ip");
-        } else {
-
-
 
                 $groupId = go_get_groupid($goUser);
 
@@ -72,69 +64,58 @@
 
                 $query = "SELECT carrier_id,carrier_name,server_ip,protocol,registration_string,active,user_group FROM vicidial_server_carriers $ul ORDER BY carrier_id LIMIT 3;";
                 $rsltv = mysqli_query($link, $query);
-                while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
-					$datacarrier_name = $fresults['carrier_name'];
-       					$datacarrier_description = $fresults['carrier_description'];
-       					$dataprotocol = $fresults['protocol'];
-       					$dataserver_ip = $fresults['server_ip'];
-                			$dataactive = $fresults['active'];
-				}
                 
                 $countResult = mysqli_num_rows($rsltv);
 
                 if($countResult > 0) {
 
-                                $items = explode("&",str_replace(";","",$values));
-                                foreach ($items as $item)
-                                {
-                                        list($var,$val) = explode("=",$item,2);
-                                        //if (strlen($val) > 0)
-                                        //{
-                                                if ($var!="carrier_id" && $var!="reg_auth" && $var!="reg_user"
-                                                        && $var!="reg_pass" && $var!="reg_host" && $var!="reg_port"
-                                                        && $var!="ip_user" && $var!="ip_pass" && $var!="ip_host"
-                                                        && $var!="allow_gsm" && $var!="allow_ulaw" && $var!="allow_alaw"
-                                                        && $var!="allow_g729" && $var!="dtmf_mode" && $var!="customDTMF"
-                                                        && $var!="dialprefix" && $var!="customProtocol")
-                                                        $itemSQL .= "$var='".str_replace('+',' ',mysqli_real_escape_string($val))."', ";
+					$items = explode("&",str_replace(";","",$values));
+					foreach ($items as $item){
+						list($var,$val) = explode("=",$item,2);
+						//if (strlen($val) > 0)
+						//{
+						if ($var!="carrier_id" && $var!="reg_auth" && $var!="reg_user"
+						&& $var!="reg_pass" && $var!="reg_host" && $var!="reg_port"
+						&& $var!="ip_user" && $var!="ip_pass" && $var!="ip_host"
+						&& $var!="allow_gsm" && $var!="allow_ulaw" && $var!="allow_alaw"
+						&& $var!="allow_g729" && $var!="dtmf_mode" && $var!="customDTMF"
+						&& $var!="dialprefix" && $var!="customProtocol")
+							$itemSQL .= "$var='".str_replace('+',' ',mysqli_real_escape_string($val))."', ";
 
-                                                if ($var=="carrier_id")
-                                                        $carrier_id="$val";
-
-                                                if ($var=="server_ip")
-                                                        $server_ip="$val";
-
-                                                if ($var=="registration_string")
-                                                        $reg_string="$val";
+						if ($var=="carrier_id")
+								$carrier_id="$val";
+						if ($var=="server_ip")
+								$server_ip="$val";
+						if ($var=="registration_string")
+								$reg_string="$val";
 						if ($var=="user_group")
 							$reg_ug="$val";
                                         //}
-                                }
+                    }
 					
-                                $reg_string = substr($reg_string,0,strpos($reg_string,":5060"));
-                                $reg_string = substr($reg_string,strrpos($reg_string,"@") + 1);
+					$reg_string = substr($reg_string,0,strpos($reg_string,":5060"));
+					$reg_string = substr($reg_string,strrpos($reg_string,"@") + 1);
 /*no effect
-                                if ($reg_string=="dal.justgovoip.com" || $reg_string=="208.43.27.84")
-                                {
-                                        $query = $this->db->query("SELECT * FROM vicidial_server_carriers WHERE carrier_id='$carrier_id' AND server_ip='$server_ip';");
-                                        $isExist = $query->num_rows();
-                                }
+					if ($reg_string=="dal.justgovoip.com" || $reg_string=="208.43.27.84")
+					{
+							$query = $this->db->query("SELECT * FROM vicidial_server_carriers WHERE carrier_id='$carrier_id' AND server_ip='$server_ip';");
+							$isExist = $query->num_rows();
+					}
 */
-                                //if (!$isExist)
-                                //{
-                                        $itemSQL = rtrim($itemSQL,', ');
-                                        if($carrier_name == null){$carrier_name = $datacarrier_name;}
-                                        if($carrier_description == null){$carrier_description = $datacarrier_description;}
-                                        if($protocol == null){$protocol = $dataprotocol;}
-                                        if($server_ip == null) {$server_ip = $dataserver_ip;}
-                                        if($active == null){$active = $dataactive;}
-                                        $query = "UPDATE vicidial_server_carriers SET  carrier_name = '$carrier_name', carrier_description = '$carrier_description', protocol = '$protocol', server_ip = '$server_ip', active = '$active' WHERE carrier_id='$carrier_id';";
+					//if (!$isExist)
+					//{
+					$itemSQL = rtrim($itemSQL,', ');
+					
+					$query = "UPDATE vicidial_server_carriers
+					SET  carrier_name = '$carrier_name', carrier_description = '$carrier_description', protocol = '$protocol',
+					server_ip = '$server_ip', active = '$active', registration_string = '$registration_string', account_entry = '$account_entry',
+					dialplan_entry = '$dialplan_entry', globals_string = '$globals_string' WHERE carrier_id='$carrier_id';";
 					$result = mysqli_query($link, $query);
                                         //var_dump("UPDATE vicidial_server_carriers SET $itemSQL WHERE carrier_id='$carrier_id';");
                                         //echo "UPDATE phones SET $itemSQL WHERE extension='$extension';";
 
-                                                $queryNew = "UPDATE servers SET rebuild_conf_files='Y' where generate_vicidial_conf='Y' and active_asterisk_server='Y' and server_ip='$server_ip';";
-						$resultNew = mysqli_query($link, $queryNew);
+					$queryNew = "UPDATE servers SET rebuild_conf_files='Y' where generate_vicidial_conf='Y' and active_asterisk_server='Y' and server_ip='$ip_address';";
+					$resultNew = mysqli_query($link, $queryNew);
 
         ### Admin logs
                                         $SQLdate = date("Y-m-d H:i:s");
@@ -142,7 +123,7 @@
                                         $rsltvLog = mysqli_query($linkgo, $queryLog);
 
 
-						$apiresults = array("result" => "success");
+						$apiresults = array("result" => "success", "query" => $query);
                                 //
                                 //      $return = "ERROR: Only one GoAutoDial-JustGoVoIP is allowed per server ip";
                                 //}
@@ -153,7 +134,7 @@
 				}
                         }
 
-}}}}}}
+}}}}
 
 
 #############################
