fecc15d1412a44fecbb598181196a52ffc3bd8f6
demian <demian@goautodial.com>
2016-12-21 17:31:38 -0500

    Bug Fix #3791

--- goInbound/goAddIVRmenu.php
+++ goInbound/goAddIVRmenu.php
@@ -11,8 +11,8 @@
     include_once ("../goFunctions.php");
 //menu_id, menu_name, user_group, menu_prompt, menu_timeout, menu_timeout_prompt, menu_invalid_prompt, menu_repeat, menu_time_check, call_time_id, track_in_vdac, custom_dialplan_entry, tracking_group 
     ### POST or GET Variables
-    $menu_id = $_REQUEST['menu_id'];
-    $menu_name = $_REQUEST['menu_name'];
+    $menu_id = mysqli_real_escape_string($link, $_REQUEST['menu_id']);
+    $menu_name = mysqli_real_escape_string($link, $_REQUEST['menu_name']);
     $user_group = $_REQUEST['user_group'];
     $menu_prompt = $_REQUEST['menu_prompt'];
     $menu_timeout = $_REQUEST['menu_timeout'];
@@ -24,15 +24,28 @@
     $track_in_vdac = $_REQUEST['track_in_vdac'];
     $custom_dialplan_entry = $_REQUEST['custom_dialplan_entry'];
     $tracking_group = $_REQUEST['tracking_group'];
-        $goUser = $_REQUEST['goUser'];
-        $ip_address = $_REQUEST['hostname'];
-    $values = $_REQUEST['items'];
+    $goUser = $_REQUEST['goUser'];
+    $ip_address = $_REQUEST['hostname'];
+		
+    $option = $_REQUEST['option'];
+	$option_desc = mysqli_real_escape_string($link, $_REQUEST['option_desc']);
+	$option_menu = $_REQUEST['option_menu'];
+	$option_value = mysqli_real_escape_string($link, $_REQUEST['option_value']);
+	$option_context = mysqli_real_escape_string($link, $_REQUEST['option_context']);
+	
+	$option = explode("+", $option);
+	$option_desc = explode("+", $option_desc);
+	$option_menu = explode("+", $option_menu);
+	$option_value = explode("+", $option_value);
+	
+	if($option_context != "" || $option_context != NULL)
+	$option_context = explode("+", $option_context);
    
     ### Default values 
 	$defmenu_time_check = array('0','1');
 	$deftrack_in_vdac = array('0','1');
 
-
+	
 ####################
 
         if($menu_id == null || strlen($menu_id) < 4) {
@@ -50,20 +63,18 @@
         if($menu_repeat == null) {
                 $apiresults = array("result" => "Error: Set a value for menu_repeat.");
         } else {
- //       if($menu_time_check == null) {
- //               $apiresults = array("result" => "Error: Set a value for menu_time_check.");
- //       } else {
-//        if($call_time_id == null) {
-//                $apiresults = array("result" => "Error: Set a value for call_time_id.");
-//        } else {
-  //      if($track_in_vdac == null) {
-   //             $apiresults = array("result" => "Error: Set a value for track_in_vdac.");
-     //   } else {
+        //if($menu_time_check == null) {
+        //        $apiresults = array("result" => "Error: Set a value for menu_time_check.");
+        //} else {
+        //if($call_time_id == null) {
+        //        $apiresults = array("result" => "Error: Set a value for call_time_id.");
+        //} else {
+        //if($track_in_vdac == null) {
+        //        $apiresults = array("result" => "Error: Set a value for track_in_vdac.");
+        //} else {
         if($tracking_group == null) {
                 $apiresults = array("result" => "Error: Set a value for tracking_group.");
         } else {
-
-
         if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $menu_id)){
                 $apiresults = array("result" => "Error: Special characters found in menu_id");
         } else {
@@ -79,28 +90,24 @@
         if(preg_match('/[\'^£$%&*()}{@#~?><>,|=_+¬-]/', $tracking_group)){
                 $apiresults = array("result" => "Error: Special characters found in tracking_group");
         } else {
-
-
-
-
-
-
         if($user_group == null) {
                 $apiresults = array("result" => "Error: Set a value for user_group.");
         } else {
 
-                $groupId = go_get_groupid($goUser);
-                if (!checkIfTenant($groupId)) {
-                        $ulug = "WHERE user_group='$user_group'";
-                } else {
-                        $ulug = "WHERE user_group='$user_group' AND user_group='$groupId'";
-                }
+		$groupId = go_get_groupid($goUser);
+		if (!checkIfTenant($groupId)) {
+				$ulug = "WHERE user_group='$user_group'";
+		} else {
+				$ulug = "WHERE user_group='$user_group' AND user_group='$groupId'";
+		}
 
-                $query = "SELECT user_group,group_name,forced_timeclock_login FROM vicidial_user_groups $ulug ORDER BY user_group LIMIT 1;";
-                $rsltv = mysqli_query($link, $query);
-                $countResult = mysqli_num_rows($rsltv);
-
-                if($countResult > 0 || $user_group == "---ALL---") {
+		$query = "SELECT user_group,group_name,forced_timeclock_login FROM vicidial_user_groups $ulug ORDER BY user_group LIMIT 1;";
+		$rsltv = mysqli_query($link, $query);
+		$countResult = mysqli_num_rows($rsltv);
+		
+		
+		
+        if($countResult > 0 || $user_group == "---ALL---") {
 
   /*              if($menu_time_check < 0 || $menu_time_check > 1) {
                         $apiresults = array("result" => "Error: menu_time_check Value should be 0 or 1");
@@ -109,77 +116,68 @@
                         $apiresults = array("result" => "Error: track_in_vdac Value should be 0 or 1");
                 } else {
 */
-                  $stmt = "SELECT value FROM vicidial_override_ids where id_table='vicidial_call_menu' and active='1';";
-                  $query = mysqli_query($link, $stmt);
-                  $voi_ct = mysqli_num_rows($query);
-
-                  if ($voi_ct > 0) {
-                          //$row = $query->row(); 
-                          //$menu_id = ($row->value + 1);
-                          $stmt = "UPDATE vicidial_override_ids SET value='$menu_id' where id_table='vicidial_call_menu' and active='1';";
-                          $result = mysqli_query($link, $stmt);
-                  }
-                  $stmtCheck = "SELECT menu_id from vicidial_call_menu where menu_id='$menu_id';";
-                  $queryCheck =  mysqli_query($link, $stmtCheck);
-                  $row = mysqli_num_rows($queryCheck);
+			$stmt = "SELECT value FROM vicidial_override_ids where id_table='vicidial_call_menu' and active='1';";
+			$query = mysqli_query($link, $stmt);
+			$voi_ct = mysqli_num_rows($query);
+	
+			if ($voi_ct > 0) {
+					//$row = $query->row(); 
+					//$menu_id = ($row->value + 1);
+					$stmt = "UPDATE vicidial_override_ids SET value='$menu_id' where id_table='vicidial_call_menu' and active='1';";
+					$result = mysqli_query($link, $stmt);
+			}
+			
+			$stmtCheck = "SELECT menu_id from vicidial_call_menu where menu_id='$menu_id';";
+			$queryCheck =  mysqli_query($link, $stmtCheck);
+			$row = mysqli_num_rows($queryCheck);
                   
-                  if ($row > 0) {
-			   $apiresults = array("result" => "Error: CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID");
-                           //$message = "CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID\n";
-                  } else {
-
-
+			if ($row > 0) {
+			  $apiresults = array("result" => "Error: CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID");
+			  //$message = "CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID\n";
+			} else {
+	
 				$queryAddIVR = "INSERT INTO vicidial_call_menu (menu_id, menu_name, user_group, menu_prompt, menu_timeout, menu_timeout_prompt, menu_invalid_prompt, menu_repeat, menu_time_check, call_time_id, track_in_vdac, custom_dialplan_entry, tracking_group) values('$menu_id', '$menu_name', '$user_group', '$menu_prompt', '$menu_timeout', '$menu_timeout_prompt', '$menu_invalid_prompt', '$menu_repeat', '$menu_time_check', '24hours', '$track_in_vdac', '$custom_dialplan_entry', '$tracking_group');";
-
-                                $resultQueryAdd = mysqli_query($link, $queryAddIVR);
-
+				$resultQueryAdd = mysqli_query($link, $queryAddIVR);
+		
 				$resultQueryAddCheck = "SELECT menu_id from vicidial_call_menu where menu_id='$menu_id';";
 				$resultCheck = mysqli_query($link, $resultQueryAddCheck);
 				$countAdd = mysqli_num_rows($resultCheck);
-                               // $apiresults = array("result" => "success");
+				
+				# set default entry in vicidial_callmenu_options by Franco Hora 
+				$queryDef = "INSERT INTO vicidial_call_menu_options (menu_id,option_value,option_description,option_route,option_route_value) values('$menu_id','TIMEOUT','Hangup','HANGUP','vm-goodbye');";
+				$resultQueryDef = mysqli_query($link, $queryDef);
+				
+				#query for call menu options
+				for($i=0; $i < count($option); $i++){
+					if($option_menu[$i] != "EXTEN")
+					$query_options = "INSERT INTO vicidial_call_menu_options (menu_id,option_value,option_description,option_route,option_route_value) values('$menu_id', '$option[$i]', '$option_desc[$i]', '$option_menu[$i]', '$option_value[$i]');";
+					else
+					$query_options = "INSERT INTO vicidial_call_menu_options (menu_id,option_value,option_description,option_route,option_route_value, option_route_value_context) values('$menu_id', '$option[$i]', '$option_desc[$i]', '$option_menu[$i]', '$option_value[$i]', '$option_context[$i]');";
+					
+					$resultQueryOptions = mysqli_query($link, $query_options);
+				}
+				
+				$queryCount = "SELECT menu_id from vicidial_call_menu_options where menu_id='$menu_id';";
+				$resultCount = mysqli_query($link, $queryCount);
+				$countOpt = mysqli_num_rows($resultCount);
+	
+				if ($countOpt > 0 && $countAdd > 0) {
+		
+		### Admin logs
+				  $SQLdate = date("Y-m-d H:i:s");
+				  $queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','ADD','Added New IVR $menu_id','INSERT INTO  vicidial_call_menu SET (menu_name, menu_id, menu_prompt, menu_timeout, menu_timeout_prompt, menu_invalid_prompt, menu_repeat, menu_time_check, call_time_id, track_in_vdac, tracking_group, user_group) VALUES ($menu_name, $menu_id, $menu_prompt, $menu_timeout, $menu_timeout_prompt, $menu_invalid_prompt, $menu_repeat, $menu_time_check, $call_time_id, $track_in_vdac, $tracking_group, $user_group);');";
+				  $rsltvLog = mysqli_query($linkgo, $queryLog);
+				  $apiresults = array("result" => "success");
+				
+				} else {
+						 $apiresults = array("result" => "CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID\n"); 
+				}
+			}
 
+		} else {
+			$apiresults = array("result" => "Error: INVALID USER GROUP");
+		}
+		
+	}}}}}}}}}}}}
 
-                           # set default entry in vicidial_callmenu_options by Franco Hora 
-			   $queryDef = "INSERT INTO vicidial_call_menu_options (menu_id,option_value,option_description,option_route,option_route_value) values('$menu_id','TIMEOUT','Hangup','HANGUP','vm-goodbye');";
-                          $resultQueryDef = mysqli_query($link, $queryDef); 
-			   
-			  $queryCount = "SELECT menu_id from vicidial_call_menu_options where menu_id='$menu_id';";
-			  $resultCount = mysqli_query($link, $queryCount);
-                  	  $countOpt = mysqli_num_rows($resultCount);
-
-                  if ($countOpt > 0 && $countAdd > 0) {
-
-        ### Admin logs
-                                        $SQLdate = date("Y-m-d H:i:s");
-                                        $queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','ADD','Added New IVR $menu_id','INSERT INTO  vicidial_call_menu SET (menu_name, menu_id, menu_prompt, menu_timeout, menu_timeout_prompt, menu_invalid_prompt, menu_repeat, menu_time_check, call_time_id, track_in_vdac, tracking_group, user_group) VALUES ($menu_name, $menu_id, $menu_prompt, $menu_timeout, $menu_timeout_prompt, $menu_invalid_prompt, $menu_repeat, $menu_time_check, $call_time_id, $track_in_vdac, $tracking_group, $user_group);');";
-
-                                        $rsltvLog = mysqli_query($linkgo, $queryLog);
-
-			   $apiresults = array("result" => "success");
-                  } else {
-                           $apiresults = array("result" => "CALL MENU NOT ADDED - there is already a CALL MENU in the system with this ID\n"); 
-		  }
-                  }
-//}}
-
-
-} else {
-	$apiresults = array("result" => "Error: INVALID USER GROUP");
-}
-}}}}}
-}}}}}
-
-}
-}
-
-
-
-
-
-
-
-
-
-
-###################3
 ?>
