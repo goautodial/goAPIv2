32c2acd9f5eeb649044c8fdedfa5a841e068183b
Chris L <chris@goautodial.com>
2017-01-25 09:12:56 +0800

    Fix for the bug on issue #3651

--- goDialStatus/getAllDialStatuses.php
+++ goDialStatus/getAllDialStatuses.php
@@ -1,27 +1,37 @@
 <?php
     #######################################################
-    #### Name: getAllCountryCodes.php	               ####
+    #### Name: getAllDialStatuses.php	               ####
     #### Description: API to get all campaigns         ####
     #### Version: 0.9                                  ####
     #### Copyright: GOAutoDial Inc. (c) 2011-2014      ####
     #### Written by: Noel Umandap                      ####
+    #### Modified by: Chris Lomuntad                   ####
     #### License: AGPLv2                               ####
     #######################################################
     include_once("../goFunctions.php");
 	
+	$hotkeys_only = mysqli_real_escape_string($_REQUEST['hotkeys_only']);
+	
+	$human_answered = '';
+	if ($hotkeys_only === "1") {
+		$human_answered = "WHERE human_answered='Y'";
+	}
+	
     $query = "SELECT status,status_name
-            FROM vicidial_statuses
-           ORDER BY status";
+				FROM vicidial_statuses
+				$human_answered
+				ORDER BY status";
    	$rsltv = mysqli_query($link, $query);
     
     while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
 		$dataStatus[] = $fresults['status'];
        	$dataStatusName[] = $fresults['status_name'];
-   		$apiresults = array(
-                        "result" => "success",
-                        "status" => $dataStatus,
-                        "status_name" => $dataStatusName,
-						"test" => $query
-                    );
 	}
+	
+	$apiresults = array(
+		"result" => "success",
+		"status" => $dataStatus,
+		"status_name" => $dataStatusName,
+		"test" => $query
+	);
 ?>
--- goHotkeys/getAllHotkeys.php
+++ goHotkeys/getAllHotkeys.php
@@ -1,52 +1,52 @@
 <?php
-   ####################################################
-   #### Name: getAllHotkeys.php                    ####
-   #### Description: API to add new list           ####
-   #### Version: 0.9                               ####
-   #### Copyright: GOAutoDial Ltd. (c) 2011-2016   ####
-   #### Written by: Noel Umandap                   ####
-   #### License: AGPLv2                            ####
-   ####################################################
-		include_once("goFunctions.php");
-   
-		$camp = $_REQUEST['hotkeyCampID'];
-		$groupId = go_get_groupid($goUser);
-		
-		if(empty($camp)) {
-				$apiresults = array("result" => "Error: Set a value for Campaign ID.");
-		} else {
-				if (!checkIfTenant($groupId)) {
-                        $ul = "";
-                } else {
-                        $ul = "AND user_group='$groupId'";
-                   $addedSQL = "WHERE user_group='$groupId'";
-                }
-				
-				$query = "SELECT
-								status,
-								hotkey,
-								status_name,
-								selectable,
-								campaign_id
-						FROM vicidial_campaign_hotkeys $ul
-						WHERE campaign_id ='$camp'
-						ORDER BY hotkey;";
-				$rsltv = mysqli_query($link,$query);
+####################################################
+#### Name: getAllHotkeys.php                    ####
+#### Description: API to add new list           ####
+#### Version: 0.9                               ####
+#### Copyright: GOAutoDial Ltd. (c) 2011-2016   ####
+#### Written by: Noel Umandap                   ####
+#### License: AGPLv2                            ####
+####################################################
+include_once("goFunctions.php");
 
-				while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
-						$dataStatus[] = $fresults['status'];
-						$dataHotkey[] = $fresults['hotkey'];
-						$dataStatusName[] = $fresults['status_name'];
-						$dataSelectable[] = $fresults['selectable'];
-						$dataCampaignID[] = $fresults['campaign_id'];
-						$apiresults = array(
-											"result" => "success",
-											"status" => $dataStatus,
-											"hotkey" => $dataHotkey,
-											"status_name" => $dataStatusName,
-											"selectable" => $dataSelectable,
-											"campaign_id" => $dataCampaignID
-										);
-				}
-		}
+$camp = $_REQUEST['hotkeyCampID'];
+$groupId = go_get_groupid($goUser);
+
+if(empty($camp)) {
+	$apiresults = array("result" => "Error: Set a value for Campaign ID.");
+} else {
+	if (!checkIfTenant($groupId)) {
+	$ul = "";
+	} else {
+	$ul = "AND user_group='$groupId'";
+	$addedSQL = "WHERE user_group='$groupId'";
+	}
+		
+	$query = "SELECT
+		status,
+		hotkey,
+		status_name,
+		selectable,
+		campaign_id
+		FROM vicidial_campaign_hotkeys $ul
+		WHERE campaign_id ='$camp'
+		ORDER BY hotkey;";
+	$rsltv = mysqli_query($link,$query);
+
+	while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
+		$dataStatus[] = $fresults['status'];
+		$dataHotkey[] = $fresults['hotkey'];
+		$dataStatusName[] = $fresults['status_name'];
+		$dataSelectable[] = $fresults['selectable'];
+		$dataCampaignID[] = $fresults['campaign_id'];
+		$apiresults = array(
+			"result" => "success",
+			"status" => $dataStatus,
+			"hotkey" => $dataHotkey,
+			"status_name" => $dataStatusName,
+			"selectable" => $dataSelectable,
+			"campaign_id" => $dataCampaignID
+		);
+	}
+}
 ?>
