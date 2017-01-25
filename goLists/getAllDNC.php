73f282e7fc0f62b290427a5d17849274b8275911
Chris L <chris@goautodial.com>
2017-01-25 13:50:06 +0800

    Fixed the query for searching DNCs

--- goLists/getAllDNC.php
+++ goLists/getAllDNC.php
@@ -3,7 +3,8 @@
     
 	$search = mysqli_real_escape_string($link,$_REQUEST['search']);
 	
-    $query = "SELECT phone_number, campaign_id from vicidial_campaign_dnc WHERE phone_number LIKE '$search%' OR campaign_id LIKE '$search%' LIMIT 1000;";
+    //$query = "SELECT phone_number, campaign_id from vicidial_campaign_dnc WHERE phone_number LIKE '$search%' OR campaign_id LIKE '$search%' LIMIT 1000;";
+	$query = "SELECT * FROM (SELECT a.phone_number AS phone_number, '' AS campaign_id FROM vicidial_dnc a UNION SELECT b.phone_number AS phone_number, b.campaign_id AS campaign_id FROM vicidial_campaign_dnc b) searchdnc where phone_number LIKE '$search%' OR campaign_id LIKE '$search%' LIMIT 1000;";
     $rsltv = mysqli_query($link, $query);
 	$countResult = mysqli_num_rows($rsltv);
     
