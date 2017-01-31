c97526af62b070dd81a353b4a1ef6b9e7791a74b
Chris L <chris@goautodial.com>
2017-01-31 09:19:18 +0800

    Updated the API for fetching admin logs

--- goAdminLogs/goGetAdminLogsList.php
+++ goAdminLogs/goGetAdminLogsList.php
@@ -1,40 +1,22 @@
 <?php
-    #######################################################
-    #### Name: goGetAdminLogsList.php	               ####
-    #### Description: API to get all admin logs        ####
-    #### Version: 0.9                                  ####
-    #### Copyright: GOAutoDial Inc. (c) 2016           ####
-    #### Written by: Alexander Jim H. Abenoja          ####
-    #### License: AGPLv2                               ####
-    #######################################################
-    
-    include_once ("goFunctions.php");
-    /*
-    $groupId = go_get_groupid($goUser);
-    
-    if (!checkIfTenant($groupId)) {
-        $ul='';
-    } else { 
-	$ul = "WHERE user_group='$groupId'";  
-  	   }*/
+#######################################################
+#### Name: goGetAdminLogsList.php	               ####
+#### Description: API to get all admin logs        ####
+#### Version: 0.9                                  ####
+#### Copyright: GOAutoDial Inc. (c) 2016           ####
+#### Written by: Alexander Jim H. Abenoja          ####
+#### Modified by: Christopher Lomuntad             ####
+#### License: AGPLv2                               ####
+#######################################################
 
-   $query = "SELECT * FROM vicidial_admin_log $ul ORDER BY admin_log_id;";
-   $rsltv = mysqli_query($link, $query);
-   //$array = array();
-   while($fresults = mysqli_fetch_array($rsltv, MYSQL_ASSOC)){
-		$dataID[] = $fresults['admin_log_id'];	 
-		$dataUser[] = $fresults['user'];
-		$dataIP[] = $fresults['ip_address'];
-		$dataDate[] = $fresults['event_date'];
-		$dataSection[] = $fresults['event_section'];
-		//$array[] = $fresults['event_type'];
-		//$array[] = $fresults['record_id'];
-		//$array[] = $fresults['event_code'];
-		//$array[] = $fresults['event_sql'];
-		//$array[] = $fresults['event_notes'];
-		//$array[] = $fresults['user_group'];
-		
-		$apiresults = array("result" => "success", "admin_log_id" => $dataID, "user" => $dataUser, "ip_address" => $dataIP, "event_date" => $dataDate);
-		//$apiresults = array_merge( array( "result" => "success" ), $array );
-	}
+if ($goUserGroup !== 'ADMIN') {
+	$goDB->where('user_group', $goUserGroup);
+}
+$adminLogs = $goDB->get('go_action_logs');
+
+foreach ($adminLogs as $log) {
+	$result[] = $log;
+}
+
+$APIResult = array( "result" => "success", "data" => $result );
 ?>
