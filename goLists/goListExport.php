9c20db3ba11d6b854f3cbb84451adb913ca72a98
Chris L <chris@goautodial.com>
2017-03-07 15:14:55 +0800

    For Export list bug

--- goLists/goListExport.php
+++ goLists/goListExport.php
@@ -95,7 +95,7 @@ ini_set('memory_limit', '2048M');
 			}
 			$explode_array = explode("|",$array_fetch);
 			$row[$x] = $explode_array;
-			if ($x > 150) {
+			if ($x > 110) {
 				var_dump($array_fetch);
 				break;
 			}
