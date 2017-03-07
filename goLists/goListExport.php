9fd1b8ff243aed6144b59d7884c59330d189db1a
Chris L <chris@goautodial.com>
2017-03-07 14:45:07 +0800

    For Export list bug

--- goLists/goListExport.php
+++ goLists/goListExport.php
@@ -87,21 +87,22 @@ ini_set('memory_limit', '2048M');
 		$u = 0;
 		$x = 0;
 		$count_header = count($header);
-		//while($fetch_row = mysqli_fetch_row($dllist)){
-		//	$array_fetch = $fetch_row[0];
-		//	$u = $u+1;
-		//	while($u < $count_header){
-		//		$array_fetch .= "|".$fetch_row[$u];
-		//		$u++;
-		//	}
-		//	$explode_array = explode("|", $array_fetch);
-		//	$row[$x] = $explode_array;
-		//	$u = 0;
-		//	$x++;
-		//}
-		$fetch_row = mysqli_fetch_row($dllist);
+		while($fetch_row = mysqli_fetch_row($dllist)){
+			var_dump($fetch_row);
+			
+			$array_fetch = $fetch_row[0];
+			$u = $u+1;
+			while($u < $count_header){
+				$array_fetch .= "|".$fetch_row[$u];
+				$u++;
+			}
+			$explode_array = explode("|", $array_fetch);
+			$row[$x] = $explode_array;
+			$u = 0;
+			$x++;
+		}
 		
-		$apiresults = array("result" => "success", "header" => $header, "row" => $fetch_row, "query" => $stmt, "query_custom_list" => $custom_table);
+		$apiresults = array("result" => "success", "header" => $header, "row" => $row, "query" => $stmt, "query_custom_list" => $custom_table);
 	}else{
 		$apiresults = array("result" => "Error: List ID not defined");
 	}
