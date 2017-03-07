721159683c8adee980434a3f5188978ec1f61da0
Chris L <chris@goautodial.com>
2017-03-07 15:43:11 +0800

    For Export list bug

--- goLists/goListExport.php
+++ goLists/goListExport.php
@@ -10,7 +10,7 @@
 /*ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);*/
-//ini_set('memory_limit', '2048M');
+ini_set('memory_limit', '2048M');
 //ini_set('memory_limit', -1);
     include_once ("../goFunctions.php");
 	
@@ -95,6 +95,9 @@ error_reporting(E_ALL);*/
 			}
 			$explode_array = explode("|",$array_fetch);
 			$row[$x] = $explode_array;
+			if ($u == 102) {
+				var_dump($row);
+			}
 			$array_fetch = "";
 			$u = 0;
 			$x++;
