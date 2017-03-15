8f9d02ad793948ac81405be6623f9e87667884e2
demian <demian@goautodial.com>
2017-03-15 18:10:16 -0400

    Multiple Delete User API

--- goUsers/goDeleteUser.php
+++ goUsers/goDeleteUser.php
@@ -10,7 +10,8 @@
     include_once ("../goFunctions.php");
     
     ### POST or GET Variables
-    $user_id = $_REQUEST['user_id'];
+    $user_id = mysqli_real_escape_string($link, $_REQUEST['user_id']);
+	$action = mysqli_real_escape_string($link, $_REQUEST['action']);
     $ip_address = $_REQUEST['hostname'];
     $goUser = $_REQUEST['goUser'];
 	
@@ -20,65 +21,106 @@
 	if($user_id == null) { 
 		$apiresults = array("result" => "Error: Set a value for User ID."); 
 	} else {
- 
-    		$groupId = go_get_groupid($goUser);
-    
-		if (!checkIfTenant($groupId)) {
-        		$ul = "AND user_id='$user_id'";
-    		} else { 
-			$ul = "AND user_id='$user_id' AND user_group='$groupId'";  
+		
+	$groupId = go_get_groupid($goUser);
+	if (!checkIfTenant($groupId)) {
+		$ul = "AND user_id='$user_id'";
+	} else { 
+		$ul = "AND user_id='$user_id' AND user_group='$groupId'";  
+	}
+	if ($groupId != 'ADMIN') {
+		$notAdminSQL = "AND user_group != 'ADMIN'";
+	}
+	
+	if($action == "delete_selected"){
+		$exploded = explode(",",$user_id);
+		$error_count = 0;
+		$string_return = "";
+		for($i=0;$i < count($exploded);$i++){
+			$selectQuery = "SELECT user,phone_login FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' AND user_id = '".$exploded[$i]."';";
+			$selectResult = mysqli_query($link, $selectQuery);
+			
+			while($fresults = mysqli_fetch_array($selectResult)){
+			$dataUserID = $fresults["user"];
+			$dataPhoneLogin = $fresults["phone_login"];
+			
+			$deleteQuery = "DELETE FROM vicidial_users WHERE user='$dataUserID' $notAdminSQL"; 
+			$deleteResult = mysqli_query($link, $deleteQuery);
+			
+			$deleteQueryGo = "DELETE FROM users WHERE name='$dataUserID'"; 
+			$deleteResultGo = mysqli_query($linkgo, $deleteQueryGo);
+			
+			$deleteQueryA = "DELETE FROM phones WHERE extension='$dataPhoneLogin';";
+			$deleteResultA = mysqli_query($link, $deleteQueryA);
+			
+			$deleteQueryCB = "DELETE FROM subscriber WHERE username='$dataPhoneLogin';";
+			$deleteResultCB = mysqli_query($linkgokam, $deleteQueryCB);
+			
+			}
+			
+			$querydel = "SELECT user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' AND user='$dataUserID' $notAdminSQL ORDER BY user ASC LIMIT 1;";
+			$rsltvdel = mysqli_query($link, $querydel);
+			$countResult = mysqli_num_rows($rsltvdel);
+			
+			if($countResult > 0) {
+				$error_count = $error_count + 1;
+			}
+			
+			$log_id = log_action($linkgo, 'DELETE', $log_user, $ip_address, "Deleted User: $dataUserID", $log_group, $deleteQuery);
+			
 		}
-		if ($groupId != 'ADMIN') {
-        		$notAdminSQL = "AND user_group != 'ADMIN'";
-                }
-
-//   		$query = "SELECT user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $ul $notAdminSQL ORDER BY user ASC LIMIT 1;";
-		$query = "SELECT user,phone_login from vicidial_users WHERE user_id='$user_id';";
-   		$rsltv = mysqli_query($link, $query);
-		$countResult = mysqli_num_rows($rsltv);
-
-		if($countResult > 0) {
-			while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
-				$dataUserID = $fresults['user'];
-				$dataPhoneLogin = $fresults['phone_login'];
-			}
-			if(!$dataUserID == null) {
-				$deleteQuery = "DELETE FROM vicidial_users WHERE user='$dataUserID' AND user != 'ADMIN'"; 
-   				$deleteResult = mysqli_query($link, $deleteQuery);
-
-				$deleteQueryGo = "DELETE FROM users WHERE name='$dataUserID'"; 
-   				$deleteResultGo = mysqli_query($linkgo, $deleteQueryGo);
-   				
-				$deleteQueryA = "DELETE FROM phones WHERE extension='$dataPhoneLogin';";
-             			$deleteResultA = mysqli_query($link, $deleteQueryA);
-				
-				$deleteQueryCB = "DELETE FROM subscriber WHERE username='$dataPhoneLogin';";
-             			$deleteResultCB = mysqli_query($linkgokam, $deleteQueryCB);
-
-### admin lgs
-                                        //$SQLdate = date("Y-m-d H:i:s");
-                                        //$queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','DELETE','Deleted User: $dataUserID','DELETE FROM vicidial_users WHERE user=$dataUserID AND user != ADMIN');";
-                                        //$rsltvLog = mysqli_query($linkgo, $queryLog);
-				//kamilio
-		                //$deleteQueryB = "DELETE FROM subscriber where username='$phone_login';";
-				//$deleteResultB = mysqli_query($deleteQueryB, $linkka);
-				        $querydel = "SELECT user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $ul $notAdminSQL ORDER BY user ASC LIMIT 1;";
-        	        		$rsltvdel = mysqli_query($link, $querydel);
-                			$countResult = mysqli_num_rows($rsltvdel);
-
-                			if($countResult > 0) {	
-						$apiresults = array("result" => "Error: Delete failed");
-					} else {
-						$log_id = log_action($linkgo, 'DELETE', $log_user, $ip_address, "Deleted User: $dataUserID", $log_group, $deleteQuery);
-						
-						$apiresults = array("result" => "success"); 
-					}
-			} else {
-				$apiresults = array("result" => "Error: User doesn't exist.");
-			}
-
+		
+		if($error_count > 0) {
+			$apiresults = array("result" => "Error: Delete Failed");
 		} else {
-			$apiresults = array("result" => "Error: User doesn't exist.");
+			$apiresults = array("result" => "success"); 
 		}
+		
+	}else{
+		
+		$selectQuery = "SELECT user,phone_login FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' AND user_id = '$user_id';";
+		$selectResult = mysqli_query($link, $selectQuery);
+		
+		while($fresults = mysqli_fetch_array($selectResult)){
+			$dataUserID = $fresults["user"];
+			$dataPhoneLogin = $fresults["phone_login"];
+		}
+		
+		$deleteQuery = "DELETE FROM vicidial_users WHERE user='$dataUserID' AND user != 'ADMIN'"; 
+		$deleteResult = mysqli_query($link, $deleteQuery);
+		
+		$deleteQueryGo = "DELETE FROM users WHERE name='$dataUserID'"; 
+		$deleteResultGo = mysqli_query($linkgo, $deleteQueryGo);
+		
+		$deleteQueryA = "DELETE FROM phones WHERE extension='$dataPhoneLogin';";
+		$deleteResultA = mysqli_query($link, $deleteQueryA);
+		
+		$deleteQueryCB = "DELETE FROM subscriber WHERE username='$dataPhoneLogin';";
+		$deleteResultCB = mysqli_query($linkgokam, $deleteQueryCB);
+		
+		$querydel = "SELECT user, full_name, user_level, user_group, active FROM vicidial_users WHERE user NOT IN ('VDAD','VDCL') AND user_level != '4' $ul $notAdminSQL ORDER BY user ASC LIMIT 1;";
+		$rsltvdel = mysqli_query($link, $querydel);
+		$countResult = mysqli_num_rows($rsltvdel);
+		
+		if($countResult > 0) {	
+		$apiresults = array("result" => "Error: Delete Failed");
+		} else {
+		$log_id = log_action($linkgo, 'DELETE', $log_user, $ip_address, "Deleted User: $dataUserID", $log_group, $deleteQuery);
+		
+		$apiresults = array("result" => "success"); 
+		}
+	}
+	
+	
+	
+	### admin lgs
+				//$SQLdate = date("Y-m-d H:i:s");
+				//$queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','DELETE','Deleted User: $dataUserID','DELETE FROM vicidial_users WHERE user=$dataUserID AND user != ADMIN');";
+				//$rsltvLog = mysqli_query($linkgo, $queryLog);
+	//kamilio
+	//$deleteQueryB = "DELETE FROM subscriber where username='$phone_login';";
+	//$deleteResultB = mysqli_query($deleteQueryB, $linkka);
+	
+
 	}
 ?>
