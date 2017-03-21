a3a0612552122097a2f0c6fd5668f1385ee5eb3a
demianb <demian@goautodial.com>
2017-03-21 16:39:52 -0400

    added 2nd round checking in settings

--- goSMTP/goSMTPActivation.php
+++ goSMTP/goSMTPActivation.php
@@ -20,7 +20,7 @@
 		$rsltv = mysqli_query($linkgo, $query);
 		$exist = mysqli_num_rows($rsltv);
 		
-		if($exist <= 0){ 
+		if($exist <= 0){
 			$new_smtp_default_query = "CREATE TABLE IF NOT EXISTS smtp_settings ( debug INT(1), timezone VARCHAR(250), ipv6_support INT(1), host VARCHAR(120), port INT(4), smtp_security VARCHAR(3), smtp_auth INT(1), username varchar(120), password varchar(120));
 			INSERT INTO smtp_settings(debug, timezone, ipv6_support, host, port, smtp_security, smtp_auth, username, password) VALUES('0', 'Etc/UTC', '0', 'smtp.gmail.com', '587', 'tls', '1', '', '');
 			ALTER TABLE `messages_outbox` ADD `external_recepient` VARCHAR(124) AFTER `user_to`;";
@@ -40,6 +40,14 @@
 			}else{
 				$apiresults = array("result" => "error", "msg" => "An error has occured, please contact the System Administrator to fix the issue.", "query" => $insert_query);
 			}
+		}
+		
+		// second check if enable smtp exists
+		$check_settings = mysqli_query($linkgo, "SELECT * FROM settings WHERE setting = 'enable_smtp';");
+		$setting_exist = mysqli_num_rows($check_settings);
+		
+		if($setting_exist <= 0){
+			$insert_enable_smtp = mysqli_query($linkgo, "INSERT INTO settings (setting, context, value) VALUES('enable_smtp', 'smtp_settings', '0');");
 		}
 		
 		$default_action = array(0, 1); // 0 = deactivate, 1 = activate
