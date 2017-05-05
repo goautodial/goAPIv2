a49eaa8e50fc232a898cd5527be0986b3e86de71
Chris L <chris@goautodial.com>
2017-05-05 16:42:40 +0800

    Minor revision

--- goAgent/goCheckIfLoggedIn.php
+++ goAgent/goCheckIfLoggedIn.php
@@ -58,6 +58,6 @@ if (!isset($task) || $task === '') {
         $rslt = $goDB->update('users', array('online', $is_online));
     }
     
-    $APIResult = array( "result" => "success", "is_online" => $is_online );
+    $APIResult = array( "result" => "success", "is_online" => $is_online, "error" => $goDB->getLastError() );
 }
 ?>
