f91b589ba90d36f1b0f5a42d36d050af4c681101
Chris L <chris@goautodial.com>
2017-05-05 17:09:23 +0800

    Minor revision

--- goAgent/goCheckIfLoggedIn.php
+++ goAgent/goCheckIfLoggedIn.php
@@ -15,7 +15,7 @@ if (isset($_GET['goTask'])) { $task = $astDB->escape($_GET['goTask']); }
 if (isset($_GET['goUpdateStatus'])) { $is_online = $astDB->escape($_GET['goUpdateStatus']); }
     else if (isset($_POST['goUpdateStatus'])) { $is_online = $astDB->escape($_POST['goUpdateStatus']); }
 
-if (!isset($task) || $task === '') {
+if (!isset($task) && $task === '') {
     $phone_settings = get_settings('phone', $astDB, $phone_login, $phone_pass);
     $extension = $phone_settings->extension;
     $protocol = $phone_settings->protocol;
