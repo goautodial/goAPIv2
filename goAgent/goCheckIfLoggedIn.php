0a912dc424bc887f51f04ae0e786827823ababe2
Chris L <chris@goautodial.com>
2017-05-05 16:28:19 +0800

    Minor revision

--- goAgent/goCheckIfLoggedIn.php
+++ goAgent/goCheckIfLoggedIn.php
@@ -10,41 +10,47 @@
 
 if (isset($_GET['goSessionName'])) { $session_name = $astDB->escape($_GET['goSessionName']); }
     else if (isset($_POST['goSessionName'])) { $session_name = $astDB->escape($_POST['goSessionName']); }
+if (isset($_GET['goCheckOnline'])) { $check_online = $astDB->escape($_GET['goCheckOnline']); }
+    else if (isset($_POST['goCheckOnline'])) { $check_online = $astDB->escape($_POST['goCheckOnline']); }
 
-$phone_settings = get_settings('phone', $astDB, $phone_login, $phone_pass);
-$extension = $phone_settings->extension;
-$protocol = $phone_settings->protocol;
-if ($protocol == 'EXTERNAL') {
-    $protocol = 'Local';
-    $extension = "{$phone_settings->dialplan_number}@{$phone_settings->ext_context}";
+if (!isset($check_online) || $check_online !== 'chat') {
+    $phone_settings = get_settings('phone', $astDB, $phone_login, $phone_pass);
+    $extension = $phone_settings->extension;
+    $protocol = $phone_settings->protocol;
+    if ($protocol == 'EXTERNAL') {
+        $protocol = 'Local';
+        $extension = "{$phone_settings->dialplan_number}@{$phone_settings->ext_context}";
+    }
+    
+    $astDB->where('sess_agent_user', $goUser);
+    $astDB->where('sess_agent_phone', $phone_login);
+    $astDB->where('sess_agent_status', 'INUSE');
+    $rslt = $astDB->getOne('go_agent_sessions');
+    $go_agent_sessions = $astDB->getRowCount();
+    
+    $astDB->where('extension', $extension);
+    $astDB->where('server_ip', $phone_settings->server_ip);
+    $astDB->where('session_name', $session_name);
+    $astDB->where('program', 'vicidial');
+    $rslt = $astDB->getOne('web_client_sessions');
+    $web_client_sessions = $astDB->getRowCount();
+    
+    $is_logged_in = 0;
+    $message = "You have been logged out from the dialer.";
+    if ($go_agent_sessions > 0 && $web_client_sessions > 0) {
+        $is_logged_in = 1;
+        $message = "You're currently logged in on the dialer.";
+    }
+
+    $APIResult = array( "result" => "success", "logged_in" => $is_logged_in, "message" => $message );
+} else {
+    $is_online = 0;
+    $goDB->where('name', $goUser);
+    $rslt = $goDB->getOne('users', 'online');
+    if ($rslt) {
+        $is_online = $rslt['online'];
+    }
+
+    $APIResult = array( "result" => "success", "is_online" => $is_online );
 }
-
-$astDB->where('sess_agent_user', $goUser);
-$astDB->where('sess_agent_phone', $phone_login);
-$astDB->where('sess_agent_status', 'INUSE');
-$rslt = $astDB->getOne('go_agent_sessions');
-$go_agent_sessions = $astDB->getRowCount();
-
-$astDB->where('extension', $extension);
-$astDB->where('server_ip', $phone_settings->server_ip);
-$astDB->where('session_name', $session_name);
-$astDB->where('program', 'vicidial');
-$rslt = $astDB->getOne('web_client_sessions');
-$web_client_sessions = $astDB->getRowCount();
-
-$is_online = 0;
-$goDB->where('name', $goUser);
-$rslt = $goDB->getOne('users', 'online');
-if ($rslt) {
-    $is_online = $rslt['online'];
-}
-
-$is_logged_in = 0;
-$message = "You have been logged out from the dialer.";
-if ($go_agent_sessions > 0 && $web_client_sessions > 0) {
-    $is_logged_in = 1;
-    $message = "You're currently logged in on the dialer.";
-}
-
-$APIResult = array( "result" => "success", "logged_in" => $is_logged_in, "message" => $message, "is_online" => $is_online );
 ?>
