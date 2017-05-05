15f4b6cf2bcae3133ebc175afc5b2277bd3b666b
Chris L <chris@goautodial.com>
2017-05-05 17:13:40 +0800

    Minor revision

--- goAgent/goCheckIfLoggedIn.php
+++ goAgent/goCheckIfLoggedIn.php
@@ -50,9 +50,7 @@ if (!isset($task) && $task === '') {
         $is_online = 0;
         $goDB->where('name', $goUser);
         $rslt = $goDB->getOne('users', 'online');
-        if ($rslt) {
-            $is_online = $rslt['online'];
-        }
+        $is_online = $rslt['online'];
     } else if ($task == 'update') {
         $goDB->where('name', $goUser);
         $rslt = $goDB->update('users', array('online' => $is_online));
