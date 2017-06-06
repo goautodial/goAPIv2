68e0ee8545b5d5685325ee186b0b70202ce2cf4f
Chris L <chris@goautodial.com>
2017-06-06 12:23:29 +0800

    Minor revision

--- goChat/goChatHistory.php
+++ goChat/goChatHistory.php
@@ -10,12 +10,17 @@
 
 if (isset($_GET['user'])) { $user = $astDB->escape($_GET['user']); }
     else if (isset($_POST['user'])) { $user = $astDB->escape($_POST['user']); }
+if (isset($_GET['goLimit'])) { $limit = $astDB->escape($_GET['goLimit']); }
+    else if (isset($_POST['goLimit'])) { $limit = $astDB->escape($_POST['goLimit']); }
+
+if (!is_numeric($limit) || $limit === '') { $limit = 50; }
+if (($user === '' && $goUser !== 'goAPI')) { $user = $goUser; }
 
 
 if (isset($user) && $user !== '') {
 	$astDB->where('sender', $user);
 	$astDB->where('recipient', $user);
-    $rslt = $astDB->get('go_chat_history');
+    $rslt = $astDB->get('go_chat_history', $limit);
     
     $APIResult = array( "result" => "success", "data" => $rslt );
 } else {
