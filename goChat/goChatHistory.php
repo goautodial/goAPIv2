9964b116ac8eef55333f727e659a9e15b984d881
Chris L <chris@goautodial.com>
2017-06-06 12:17:00 +0800

    Minor revision

--- goChat/goChatHistory.php
+++ goChat/goChatHistory.php
@@ -8,15 +8,17 @@
 #### License: AGPLv2                            ####
 ####################################################
 
-if (isset($_GET['goRecipient'])) { $recipient = $astDB->escape($_GET['goRecipient']); }
-    else if (isset($_POST['goRecipient'])) { $recipient = $astDB->escape($_POST['goRecipient']); }
+if (isset($_GET['user'])) { $user = $astDB->escape($_GET['user']); }
+    else if (isset($_POST['user'])) { $user = $astDB->escape($_POST['user']); }
 
 
-if (isset($recipient) && $recipient !== '') {
-	$astDB->where('sender', $recipient);
-	$astDB->where('recipient', $recipient);
+if (isset($user) && $user !== '') {
+	$astDB->where('sender', $user);
+	$astDB->where('recipient', $user);
     $rslt = $astDB->get('go_chat_history');
     
     $APIResult = array( "result" => "success", "data" => $rslt );
+} else {
+	$APIResult = array( "result" => "error", "message" => "Field 'user' should not be empty." );
 }
 ?>
