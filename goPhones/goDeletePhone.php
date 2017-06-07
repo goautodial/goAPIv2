0f8e0e69d163db80d9c205325818d8503976459b
demian <demian@goautodial.com>
2017-06-07 15:14:53 -0400

    added multiple delete in phones

--- goPhones/goDeletePhone.php
+++ goPhones/goDeletePhone.php
@@ -11,59 +11,85 @@
     
     ### POST or GET Variables
     $extension = $_REQUEST['extension'];
-        $goUser = $_REQUEST['goUser'];
-        $ip_address = $_REQUEST['hostname'];
+	$action = mysqli_real_escape_string($link, $_REQUEST['action']);
+	$ip_address = $_REQUEST['hostname'];
 	
-	$log_user = mysqli_real_escape_string($link, $_REQUEST['log_user']);
-	$log_group = mysqli_real_escape_string($link, $_REQUEST['log_group']);
+	$groupId = go_get_groupid($session_user);
+	$log_user = $session_user;
+	$log_group = $groupId;
     
     ### Check campaign_id if its null or empty
 	if($extension == null) { 
 		$apiresults = array("result" => "Error: Set a value for Extension."); 
 	} else {
- 
-    		$groupId = go_get_groupid($goUser);
-    
 		if (!checkIfTenant($groupId)) {
-        		$ul = "WHERE extension='$extension'";
-    		} else { 
-			$ul = "WHERE extension='$extension' AND user_group='$groupId'";  
+			$ul = "WHERE extension='$extension'";
+		} else {
+			$ul = "WHERE extension='$extension' AND user_group='$groupId'";
 		}
-
-   		$query = "SELECT extension  FROM phones $ul ORDER BY extension LIMIT 1;";
-   		$rsltv = mysqli_query($link,$query);
-		$countResult = mysqli_num_rows($rsltv);
-
-		if($countResult > 0) {
-			while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
-				$dataExtension = $fresults['extension'];
-			}
-
-			if(!$dataExtension == null) {
-				$deleteQuery = "DELETE FROM phones WHERE extension = '$dataExtension'"; 
-   				$deleteResult = mysqli_query($link,$deleteQuery);
-				//echo $deleteQuery;
-				$deleteQueryB = "DELETE FROM subscriber where username= '$dataExtension'"; 
-   				$deleteResultB = mysqli_query($linkgokam,$deleteQueryB);
-
-				/* Create connection to kamilioDB
-                                $kamquery = "DELETE FROM subscriber where username='$exten';";
-                                $this->kamilioDB->query($kamquery);
-				*/
-
-        ### Admin logs
-                                        //$SQLdate = date("Y-m-d H:i:s");
-                                        //$queryLog = "INSERT INTO go_action_logs (user,ip_address,event_date,action,details,db_query) values('$goUser','$ip_address','$SQLdate','DELETE','Deleted New Phone $extension','DELETE FROM phones WHERE extension = $dataExtension;');";
-                                        //$rsltvLog = mysqli_query($linkgo,$queryLog);
+		
+		if($action == "delete_selected"){
+			$exploded = explode(",",$extension);
+			$error_count = 0;
+			$string_return = "";
+			for($i=0;$i < count($exploded);$i++){
+				$query = "SELECT extension  FROM phones WHERE extension='".$exploded[$i]."';";
+				$rsltv = mysqli_query($link,$query);
+				
+				while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
+					$dataExtension = $fresults['extension'];
+					$deleteQuery = "DELETE FROM phones WHERE extension = '$dataExtension'"; 
+					$deleteResult = mysqli_query($link,$deleteQuery);
+					//echo $deleteQuery;
+					$deleteQueryB = "DELETE FROM subscriber where username= '$dataExtension'"; 
+					$deleteResultB = mysqli_query($linkgokam,$deleteQueryB);
+				}
+				
+				$query = "SELECT extension  FROM phones  WHERE extension='".$dataExtension."';";
+				$rslta = mysqli_query($link,$query);
+				$countResult = mysqli_num_rows($rslta);
+				
+				if($countResult > 0) {
+					$error_count = $error_count + 1;
+				}
+				
 				$log_id = log_action($linkgo, 'DELETE', $log_user, $ip_address, "Deleted Phone $extension", $log_group, $deleteQuery);
-
-				$apiresults = array("result" => "success");
+					
+			}
+			
+			if($error_count > 0) {
+				$apiresults = array("result" => "Error: Delete Failed");
+			} else {
+				$apiresults = array("result" => "success"); 
+			}
+			
+		}else{
+			$query = "SELECT extension  FROM phones $ul ORDER BY extension LIMIT 1;";
+			$rsltv = mysqli_query($link,$query);
+			
+			if($rsltv) {
+				while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
+					$dataExtension = $fresults['extension'];
+					$deleteQuery = "DELETE FROM phones WHERE extension = '$dataExtension'"; 
+					$deleteResult = mysqli_query($link,$deleteQuery);
+					//echo $deleteQuery;
+					$deleteQueryB = "DELETE FROM subscriber where username= '$dataExtension'"; 
+					$deleteResultB = mysqli_query($linkgokam,$deleteQueryB);
+				}
+				
+				$query = "SELECT extension  FROM phones $ul ORDER BY extension LIMIT 1;";
+				$rsltv = mysqli_query($link,$query);
+				$countResult = mysqli_num_rows($rsltv);
+				
+				if($countResult > 0) {
+					$error_count = $error_count + 1;
+				}
+					$log_id = log_action($linkgo, 'DELETE', $log_user, $ip_address, "Deleted Phone $extension", $log_group, $deleteQuery);
+					$apiresults = array("result" => "success");
+					
 			} else {
 				$apiresults = array("result" => "Error: Extension doesn't exist.");
 			}
-
-		} else {
-			$apiresults = array("result" => "Error: Extension doesn't exist.");
 		}
 	}//end
 ?>
