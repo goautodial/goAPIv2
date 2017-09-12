265a94bd63a1a61e4bca131e911db861f9c0fbd6
root <dev@goautodial.com>
2017-09-12 15:35:16 -0400

    Adjustments copy custom fields

--- goCustomFields/goCopyCustomFields.php
+++ goCustomFields/goCopyCustomFields.php
@@ -185,6 +185,8 @@
         $rsltv = mysqli_query($link, $query);
         
         $output = array();
+        $output1 = array();
+        $field_sql ='';
         while($fresults = mysqli_fetch_array($rsltv, MYSQLI_ASSOC)){
             $field_label       = $fresults['field_label'];
             $field_name        = $fresults['field_name'];
@@ -222,12 +224,13 @@
                     $field_options_array = explode("\n",$field_options);
                     $field_options_count = count($field_options_array);
                     $te=0;
+                    $field_options_ENUM='';
                     while ($te < $field_options_count)
                         {
                         if (preg_match("/,/",$field_options_array[$te]))
                             {
                             $field_options_value_array = explode(",",$field_options_array[$te]);
-                            $field_options_ENUM .= str_replace(" ","_",$field_options_value_array[0].",");
+                            $field_options_ENUM .= str_replace(" ","_","'".$field_options_value_array[0]."',");
                             }
                         $te++;
                        }
@@ -291,15 +294,16 @@
                     $field_sql .="";  
                 }
                 
-                if ($queryCheckTable) {
+                if ($tableExist > 0) {
                     $field_sql .= ";";
                 } else {
                     $field_sql .= ");";
                 }
                 $stmtCUSTOM="$field_sql";
-                // $output[] = $field_sql;
+                // $output1[] = $field_sql;
                 $rslt = mysqli_query($link, $stmtCUSTOM);
-
+                $field_sql = '';
+                // $output[] = mysqli_error($link);
                 $insert = "INSERT INTO vicidial_lists_fields
                             set field_label='$field_label',
                                 field_name='$field_name',
@@ -332,7 +336,7 @@
                 }
             }
         }
-
+        // $apiresults = array("result" => "success", "query" => $output, "query1" => $output1);
         if(in_array("error", $output)){
             $apiresults = array("result" => "success", "data" => "some fields are detected as duplicate and skipped");
         }else{
