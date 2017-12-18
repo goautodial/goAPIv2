b8c6442e37b4c8c9a1746d02ce6379f17e2e53a1
Chris Lomuntad <chris@iknowitworks.org>
2017-12-19 02:40:21 +0800

    Updated query for Issue #6062

--- goAgent/goCheckIfLoggedIn.php
+++ goAgent/goCheckIfLoggedIn.php
@@ -39,24 +39,25 @@ if (!isset($task) || (isset($task) && $task === '')) {
     $rslt = $astDB->getOne('web_client_sessions');
     $web_client_sessions = $astDB->getRowCount();
     
-    $last_call_is_null = 0;
+    $last_call_is_not_null = 1;
     $added_message = '';
     if ($check_last_call) {
         //$stmt = "SELECT * FROM vicidial_agent_log WHERE user='$goUser' AND (lead_id IS NOT NULL AND lead_id > '0') AND pause_sec >= '65535' AND status IS NULL AND sub_status IS NULL AND event_time >= NOW() - INTERVAL 30 MINUTE ORDER BY event_time DESC LIMIT 1;";
         $stmt = "SELECT * FROM vicidial_agent_log val LEFT JOIN vicidial_log vl ON val.uniqueid=vl.uniqueid AND val.lead_id!=vl.lead_id WHERE val.user='$goUser' AND (val.lead_id IS NOT NULL AND val.lead_id > '0') AND val.pause_sec >= '65535' AND val.status IS NULL AND val.sub_status IS NULL AND val.event_time >= NOW() - INTERVAL 30 MINUTE ORDER BY val.event_time DESC LIMIT 1;";
+        $stmt = "SELECT val.lead_id,vla.lead_id,vl.lead_id,val.user,val.event_time,val.uniqueid,vl.uniqueid FROM vicidial_agent_log val INNER JOIN vicidial_log vl ON vl.lead_id=val.lead_id AND vl.user=val.user INNER JOIN vicidial_live_agents vla ON val.lead_id=vla.lead_id AND val.user=vla.user WHERE val.user='$goUser' AND (val.lead_id IS NOT NULL AND val.lead_id > '0') AND vla.status='INCALL' ORDER BY val.event_time DESC,vl.call_date DESC LIMIT 1;";
         $rslt = $astDB->rawQuery($stmt);
-        $last_call_is_null = $astDB->getRowCount();
-        $added_message = " There was a problem with your session. Reloading the dialer.";
+        $last_call_is_not_null = $astDB->getRowCount();
+        $added_message = " There was a problem with your session. Please re-login or reload your browser.";
     }
     
     $is_logged_in = 0;
     $message = "You have been logged out from the dialer.{$added_message}";
-    if ($go_agent_sessions > 0 && $web_client_sessions > 0 && $last_call_is_null < 1) {
+    if ($go_agent_sessions > 0 && $web_client_sessions > 0 && $last_call_is_not_null > 0) {
         $is_logged_in = 1;
         $message = "You're currently logged in on the dialer.";
     }
 
-    $APIResult = array( "result" => "success", "logged_in" => $is_logged_in, "last_call_is_null" => $last_call_is_null, "message" => $message );
+    $APIResult = array( "result" => "success", "logged_in" => $is_logged_in, "last_call_is_not_null" => $last_call_is_not_null, "message" => $message );
 } else {
     if ($task == 'check') {
         $is_online = 0;
