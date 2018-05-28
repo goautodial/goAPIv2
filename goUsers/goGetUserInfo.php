5edefcf5d87bb49d6eb2878fcee8b91377c5d23b
unknown <alexandero.oabenoja@gmail.com>
2018-05-29 01:59:39 +0800

    Header Licenses

--- goUsers/goGetUserInfo.php
+++ goUsers/goGetUserInfo.php
@@ -1,14 +1,26 @@
 <?php
-    /************************************************
-    * Name: goGetUserInfo.php                       *
-    * Description: API to get specific user         *
-    * Version: 0.9                                  *
-    * Copyright: GOAutoDial Inc. (c) 2011-2014      *
-    * Written by: Jeremiah Sebastian Samatra        *
-    *             Demian Lizandro Biscocho          *
-    * License: AGPLv2                               *
-    ************************************************/
-    include_once("../goFunctions.php");
+/**
+ * @file        goGetUserInfo.php
+ * @brief       API to get specific user details 
+ * @copyright   Copyright (C) GOautodial Inc.
+ * @author      Alexander Jim H. Abenoja <alex@goautodial.com>
+ * @author      Demian Lizandro Biscocho <demianb@goautodial.com>
+ *
+ * @par <b>License</b>:
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
+    include_once ("goAPI.php");
 
     // POST or GET Variables
     $user_id = $_REQUEST['user_id'];
