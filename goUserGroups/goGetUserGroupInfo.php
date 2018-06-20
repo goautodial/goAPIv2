b39f40d1d4db9f951a469dec6ca81577a6c22bdf
Demian Biscocho <demian@goautodial.com>
2018-06-20 13:03:32 -0400

    Old Pattern for Permissions

--- goUserGroups/goGetUserGroupInfo.php
+++ goUserGroups/goGetUserGroupInfo.php
@@ -47,15 +47,15 @@
 		
 		$cols = array("user_group", "group_name", "allowed_campaigns", "forced_timeclock_login", "shift_enforcement", "admin_viewable_groups");
 		$astDB->orderBy("user_group","asc");
-		$astQuery = $astDB->getOne("vicidial_user_groups", NULL, $cols);
+		$astQuery = $astDB->get("vicidial_user_groups", NULL, $cols);
    		//$query = "SELECT user_group,group_name,forced_timeclock_login,shift_enforcement,allowed_campaigns,admin_viewable_groups FROM vicidial_user_groups $ul ORDER BY user_group LIMIT 1;";\
 		
 		$cols2 = array("group_level", "permissions");
 		$goDB->where("user_group", $user_group);
-		$goQuery = $goDB->getOne("user_access_group", NULL, $cols2);
+		$goQuery = $goDB->get("user_access_group", NULL, $cols2);
 		//$queryGL = "SELECT group_level,permissions FROM user_access_group WHERE user_group='$user_group';";
 		
-		$data = array($astQuery, $goQuery);
+		$data = array_merge($astQuery[0], $goQuery[0]);
 		
 		$log_id = log_action($goDB, 'VIEW', $log_user, $ip_address, "Viewed the info of User Group: $user_group", $log_group);
 		
