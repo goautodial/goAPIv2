313e137a9c9909803bfb712dfd806acefc298fea
demianb <demian@goautodial.com>
2018-06-22 02:04:54 +0800

    Code cleanup..

--- goCampaigns/goGetAllCampaigns.php
+++ goCampaigns/goGetAllCampaigns.php
@@ -23,23 +23,26 @@
 	include_once ("goAPI.php");
 	$agent = get_settings('user', $astDB, $goUser);
 
-	if(!empty($agent)){
-		if(!empty($agent->user_group)){
+	$log_user = $session_user;
+	$log_group = go_get_groupid($session_user, $astDB);
+	
+	if(!empty($log_user)){
+		if(!empty($log_group)){
 			// Getting Allowed Campigns
-			$astDB->where('user_group', $agent->user_group);
+			$astDB->where('user_group', $log_group);
 			$allowedCamp = $astDB->getOne('vicidial_user_groups', "TRIM(REPLACE(allowed_campaigns,' -','')) AS allowed_campaigns");
 
-			if (checkIfTenant($agent->user_group, $goDB)) {
+			if (checkIfTenant($log_group, $goDB)) {
 				//do nothing
 			} else {
-				if($agent->user_group !== "ADMIN"){
-					$astDB->where('user_group', $agent->user_group);
+				if($log_group !== "ADMIN"){
+					$astDB->where('user_group', $log_group);
 				}
 			}
 			
 			if (isset($user_group) && strlen($user_group) > 0) {
 				if (!preg_match("/ALLCAMPAIGNS/",  $allowedCamp['allowed_campaigns'])) {
-					$cl = explode(' ', $query['allowed_campaigns']);
+					$cl = explode(' ', $allowedCamp['allowed_campaigns']);
     				$astDB->where('campaign_id', $cl, 'in');
 				}
 			}
