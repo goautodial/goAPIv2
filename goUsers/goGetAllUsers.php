50efdc54570b9b1dcafc03a73018edc07dc8a3c4
demianb <demian@goautodial.com>
2018-06-21 19:38:30 +0800

    Code cleanup..

--- goUsers/goGetAllUsers.php
+++ goUsers/goGetAllUsers.php
@@ -19,7 +19,7 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 **/
-    @include_once ("goAPI.php");
+    include_once ("goAPI.php");
     include_once ("../licensed-conf.php");
 	
 	$log_user = $session_user;
