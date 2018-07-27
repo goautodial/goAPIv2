3cbd32733e5d1e2b9bf7343294872b2b40bb6192
demianb <demian@goautodial.com>
2018-07-27 11:37:22 +0800

    Code cleanup..

--- goVoiceFiles/goAddVoiceFiles.php
+++ goVoiceFiles/goAddVoiceFiles.php
@@ -21,101 +21,111 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-// POST or GET Variables
-/*$audiofiles = $_REQUEST['files']*/;
-$stage = $astDB->escape($_REQUEST['stage']);
-$groupId = go_get_groupid($session_user, $astDB);
-$log_group = $groupId;
-$ip_address = $astDB->escape($_REQUEST['hostname']);
-
-### Default values 
-$audiofile_name=$_FILES["files"]['name'];
-$WeBServeRRooT = '/var/lib/asterisk/';
-$sounds_web_directory = 'sounds';
-$audiofile=$_FILES["files"];
-$audiofile_orig = $_FILES['files']['name'];
-$audiofile_dir = $_FILES['files']['tmp_name'];
-$audiofile_size = $_FILES['files']['size'];
-$server_name = getenv("SERVER_NAME");
-$web_server_ip = getenv("SERVER_ADDR");
-
-if($path_sounds === "" || !isset($path_sounds) || $path_sounds === NULL)
-	$path_sounds = $WeBServeRRooT.$sounds_web_directory;
+    include_once("goAPI.php");
 	
-//die($path_sounds);
-
-if ($stage == "upload") {
-	//$audiofile
-	$explodefile = explode(".",strtolower($audiofile_orig));
-	$prefix = (checkIfTenant($groupId, $goDB)) ? "go_".$groupId."_" : "go_";
+	$log_user 									= $session_user;
+	$log_group 									= go_get_groupid($log_user, $astDB); 
+	$log_ip 									= $astDB->escape($_REQUEST['log_ip']);	
 	
-	if (preg_match("/\.(wav|mp3)$/i",$audiofile_orig)) {
-		$audiofile_dir = preg_replace("/ /",'\ ',$audiofile_dir);
-		$audiofile_dir = preg_replace("/@/",'\@',$audiofile_dir);
-		$audiofile_name = preg_replace("/ /",'',"$prefix".$audiofile_name);
-		$audiofile_name = preg_replace("/@/",'',$audiofile_name);
-		
-		$audio_filesize = formatSizeUnits($audiofile_size);
-		
-		$encoded_audio = base64_encode($audiofile_name);
-		
-		//$get_sounds = "SELECT * FROM sounds WHERE goFilename = '$audiofile_name' AND goDirectory = '$path_sounds';";
-		$goDB->where('goFIlename', $audiofile_name);
-		$goDB->where('goDirectory', $path_sounds);
-		$exec_get_sounds = $goDB->get('sounds');
-		$count_sounds = $goDB->getRowCount();
-		
-		if($count_sounds <= 0){
-			copy($audiofile_dir, "$path_sounds/$audiofile_name");
-			chmod("$path_sounds/$audiofile_name", 0644);
-			if (file_exists("$path_sounds/$audiofile_name")) {
-				//$query_sounds = "INSERT INTO sounds(goFilename, goDirectory, goFileDate, goFilesize, uploaded_by) VALUES('$audiofile_name', '$path_sounds', NOW(), '$audio_filesize', '$session_user');";
-				$insertData = array(
-					'goFilename' => $audiofile_name,
-					'goDirectory' => $path_sounds,
-					'goFileDate' => 'NOW()',
-					'goFilesize' => $audio_filesize,
-					'uploaded_by' => $session_user
-				);
-				$exec_sounds = $goDB->insert('sounds', $insertData);
-				
-				//$query_go_sounds = "INSERT INTO go_sounds('type', 'data') VALUES('audio/mp3/wav', '$encoded_audio')";
-				//$exec_go_sounds = mysqli_query($linkgo, $query_go_sounds);
-				
-				// Log
-				$log_id = log_action($goDB, 'UPLOAD', $log_user, $ip_address, "Uploaded New Voice File: $audiofile_name", $log_group);
-				
-				//$stmt = "SELECT server_id,server_ip,active,server_description FROM servers";
-				$servers = $astDB->get('servers');
-				foreach ($servers as $fresults) {
-					$server_ip = $fresults['server_ip'];
-					$active = $fresults['active'];
-					$server_description = $fresults['server_description'];
-					#if(!preg_match('/dialer/i',$server_description) && $active == "Y"){
-					if($active == "Y"){
-						//exec('/usr/share/goautodial/goautodialc.pl "rsync -avz -e \"ssh -p222\" /var/lib/asterisk/sounds/'.$audiofile_name.' root@'.$server_ip.':/var/lib/asterisk/sounds"');
-						exec('/usr/share/goautodial/goautodialc.pl "rsync -avz -e \"ssh -p222\" '.$path_sounds.'/'.$audiofile_name.' root@'.$server_ip.':'.$path_sounds.'"');
-					}
-				}
-				if($exec_sounds) {
-					$apiresults = array("result" => "success");
-					//$stmtUpdate = "UPDATE servers SET sounds_update='Y';";
-					$rsltUpdate = $astDB->update('servers', array('sounds_update' => 'Y'));
-				} else {
-					//$data['uploadfail'] = "{$this->lang->line("go_file_type_wav")}";
-					$apiresults = array("result" => "error");
-				}
-			} else {
-				//$data['uploadfail'] = "{$this->lang->line("go_file_type_wav")}";
-				$apiresults = array("result" => "error");
-			}
-		} else {
-			$apiresults = array("result" => "exists");
+	// POST or GET Variables
+	/*$audiofiles = $_REQUEST['files']*/;
+	$stage 										= $astDB->escape($_REQUEST['stage']);
+
+	### Default values 
+	$audiofile_name								= $_FILES['files']['name'];
+	$WeBServeRRooT 								= '/var/lib/asterisk/';
+	$sounds_web_directory 						= 'sounds';
+	$audiofile									= $_FILES['files'];
+	$audiofile_orig 							= $_FILES['files']['name'];
+	$audiofile_dir 								= $_FILES['files']['tmp_name'];
+	$audiofile_size 							= $_FILES['files']['size'];
+	$server_name 								= getenv("SERVER_NAME");
+	$web_server_ip 								= getenv("SERVER_ADDR");
+
+    // Error Checking
+	if (empty($log_user) || is_null($log_user)) {
+		$apiresults 							= array(
+			"result" 								=> "Error: Session User Not Defined."
+		);
+	} else {		
+		if ( $path_sounds === "" || !isset($path_sounds) || $path_sounds === NULL ) {
+			$path_sounds 						= $WeBServeRRooT.$sounds_web_directory;
 		}
-		//if ($web_server_ip == "162.216.5.169") {
-		//    exec('/usr/share/goautodial/goautodialc.pl "rsync -avz -e \"ssh -p222\" /var/lib/asterisk/sounds/'.$audiofile_name.' root@162.216.5.164:/var/lib/asterisk/sounds"');
-		//}
+		
+		if ( $stage == "upload") {
+			//$audiofile
+			$explodefile 						= explode(".",strtolower($audiofile_orig));
+			$prefix 							= ( checkIfTenant($log_group, $goDB) ) ? "go_".$log_group."_" : "go_";
+			
+			if ( preg_match("/\.(wav|mp3)$/i",$audiofile_orig) ) {
+				$audiofile_dir 					= preg_replace("/ /",'\ ',$audiofile_dir);
+				$audiofile_dir 					= preg_replace("/@/",'\@',$audiofile_dir);
+				$audiofile_name 				= preg_replace("/ /",'',"$prefix".$audiofile_name);
+				$audiofile_name 				= preg_replace("/@/",'',$audiofile_name);			
+				$audio_filesize 				= formatSizeUnits($audiofile_size);			
+				$encoded_audio 					= base64_encode($audiofile_name);
+				
+				//$get_sounds = "SELECT * FROM sounds WHERE goFilename = '$audiofile_name' AND goDirectory = '$path_sounds';";
+				$goDB->where('goFIlename', $audiofile_name );
+				$goDB->where('goDirectory', $path_sounds );
+				$exec_get_sounds				= $goDB->get( 'sounds' );
+				$count_sounds 					= $goDB->getRowCount();
+				
+				if ( $count_sounds <= 0 ) {
+					copy( $audiofile_dir, "$path_sounds/$audiofile_name" );
+					chmod( "$path_sounds/$audiofile_name", 0644 );
+					if ( file_exists("$path_sounds/$audiofile_name") ) {
+						//$query_sounds = "INSERT INTO sounds(goFilename, goDirectory, goFileDate, goFilesize, uploaded_by) VALUES('$audiofile_name', '$path_sounds', NOW(), '$audio_filesize', '$session_user');";
+						$insertData 			= array(
+							'goFilename' 			=> $audiofile_name,
+							'goDirectory' 			=> $path_sounds,
+							'goFileDate' 			=> 'NOW()',
+							'goFilesize' 			=> $audio_filesize,
+							'uploaded_by' 			=> $session_user
+						);
+						
+						$exec_sounds 			= $goDB->insert('sounds', $insertData);
+						// Log
+						$log_id 				= log_action($goDB, 'UPLOAD', $log_user, $log_ip, "Uploaded New Voice File: $audiofile_name", $log_group);
+						
+						$servers 				= $astDB->get('servers');
+						
+						foreach ($servers as $fresults) {
+							$server_ip 			= $fresults['server_ip'];
+							$active 			= $fresults['active'];
+							$server_description = $fresults['server_description'];
+							#if(!preg_match('/dialer/i',$server_description) && $active == "Y"){
+							if($active == "Y"){
+								//exec('/usr/share/goautodial/goautodialc.pl "rsync -avz -e \"ssh -p222\" /var/lib/asterisk/sounds/'.$audiofile_name.' root@'.$server_ip.':/var/lib/asterisk/sounds"');
+								//exec('/usr/share/goautodial/goautodialc.pl "rsync -avz -e \"ssh -p222\" '.$path_sounds.'/'.$audiofile_name.' root@'.$server_ip.':'.$path_sounds.'"');
+							}
+						}
+						
+						if ( $exec_sounds ) {
+							$apiresults 		= array(
+								"result" 			=> "success"
+							);
+							//$stmtUpdate = "UPDATE servers SET sounds_update='Y';";
+							$rsltUpdate 		= $astDB->update( 'servers', array('sounds_update' => 'Y') );
+						} else {
+							//$data['uploadfail'] = "{$this->lang->line("go_file_type_wav")}";
+							$apiresults 		= array(
+								"result" 			=> "error"
+							);
+						}
+					} else {
+						//$data['uploadfail'] = "{$this->lang->line("go_file_type_wav")}";
+						$apiresults				 = array(
+							"result" 				=> "error"
+						);
+					}
+				} else {
+					$apiresults 				= array(
+						"result" 					=> "exists"
+					);
+				}
+			}
+		}
 	}
-}
-//}
+
 ?>
--- goVoiceFiles/goGetVoiceFileInfo.php
+++ goVoiceFiles/goGetVoiceFileInfo.php
@@ -21,105 +21,148 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-### POST or GET Variables
-$audiofile = $astDB->escape($_REQUEST['audiofile']);
-$user = $astDB->escape($_REQUEST['user']);
-
-if($audiofile == null) {
-	$apiresults = array("result" => "Error: Set a value for Audiofile");
-} else {
-    //$stmt = "SELECT count(*) as countuser from vicidial_users where user='$user' and user_level > 6;";
-	$astDB->where('user', $user);
-	$astDB->where('user_level', '6', '>');
-	$rsltv = $astDB->get('vicidial_users');
-    //$rslt = $this->asteriskDB->query($stmt);
-	$allowed_user = $astDB->getRowCount(); 
-	if ($allowed_user < 1) {
-		$result = 'ERROR';
-		$result_reason = "sounds_list USER DOES NOT HAVE PERMISSION TO VIEW SOUNDS LIST";
-		$apiresults = array("result" => "Error".$result_reason);
-	} else {
-		$server_name = getenv("SERVER_NAME");
-		$server_port = getenv("SERVER_PORT");
-		if (eregi("443",$server_port)) {$HTTPprotocol = 'https://';}
-		else {$HTTPprotocol = 'http://';}
-		$admDIR = "$HTTPprotocol$server_name:$server_port";
-		
-		#############################################
-		##### START SYSTEM_SETTINGS LOOKUP #####
-		//$stmtOne = "SELECT use_non_latin,sounds_central_control_active,sounds_web_server,sounds_web_directory FROM system_settings;";
-		$rslt = $astDB->getOne('system_settings');
-		$ss_conf_ct = $astDB->getRowCount();
-		
-		if ($ss_conf_ct > 0) {
-			$non_latin = $fresults['use_non_latin'];
-			$sounds_central_control_active = $fresults['sounds_central_control_active'];
-			$sounds_web_server = $fresults['sounds_web_server'];
-			$sounds_web_directory = $fresults['sounds_web_directory'];
-		}
-		##### END SETTINGS LOOKUP #####
-		###########################################
-		
-		if ($sounds_central_control_active < 1) {
-			$result = 'ERROR';
-			$result_reason = "sounds_list CENTRAL SOUND CONTROL IS NOT ACTIVE";
-			$apiresults = array("result" => "Error: ".$result_reason);
-		} else {
-			$i = 0;
-			$filename_sort = $MT;
-			#$dirpath = "$WeBServeRRooT/$sounds_web_directory";
-			$dirpath = "/var/lib/asterisk/sounds";
-			$dh = opendir($dirpath);
+    include_once ("goAPI.php");
 	
-			// if ($DB>0) {echo "DEBUG: sounds_list variables - $dirpath|$stage|$format\n";}
-			while (false !== ($file = readdir($dh))) {
-				# Do not list subdirectories
-				if ( (!is_dir("$dirpath/$file")) and (preg_match('/\.(wav|mp3)$/', $file)) ) {
-					if ((!is_null($search) && strlen($search) > 0)) {
-						if (!preg_match("/$search/", $file))
-							continue;
-					}
-					if (file_exists("$dirpath/$file")) {
-						//      'sample_prompt','date',30;
-						$stage = "date";
+	$log_user 										= $session_user;
+	$log_group 										= go_get_groupid($session_user, $astDB); 
+	$log_ip 										= $astDB->escape($_REQUEST['log_ip']);	
+	
+	### POST or GET Variables
+	$audiofile 										= $astDB->escape($_REQUEST['audiofile']);
+	$user 											= $astDB->escape($_REQUEST['user']);
 
-						$file_names[$i] = $file;
-						$file_namesPROMPT[$i] = preg_replace("/\.wav$|\.gsm$|\.mp3$/","",$file);
-						$file_epoch[$i] = filemtime("$dirpath/$file");
-						$file_dates[$i] = date ("Y-m-d H:i:s", filemtime("$dirpath/$file"));
-						$file_sizes[$i] = filesize("$dirpath/$file");
-						$file_sizesPAD[$i] = sprintf("[%020s]\n",filesize("$dirpath/$file"));
-						if (eregi('date',$stage)) {$file_sort[$i] = $file_epoch[$i] . "----------" . $i;}
-						if (eregi('name',$stage)) {$file_sort[$i] = $file_names[$i] . "----------" . $i;}
-						if (eregi('size',$stage)) {$file_sort[$i] = $file_sizesPAD[$i] . "----------" . $i;}
+    // Error Checking
+	if ( empty($log_user) || is_null($log_user) ) {
+		$apiresults 								= array(
+			"result" 									=> "Error: Session User Not Defined."
+		);
+	} elseif ( $audiofile == null ) {
+		$apiresults 								= array(
+			"result" 									=> "Error: Set a value for Audiofile"
+		);
+	} else {
+		//$stmt = "SELECT count(*) as countuser from vicidial_users where user='$user' and user_level > 6;";
+		if ( checkIfTenant($log_group, $astDB) ) {
+			$astDB->where( "user	_group", $log_group );
+		}
+		
+		$astDB->where( 'user', $user );
+		$astDB->where(' user_level', '6', '>' );
+		$astDB->get( 'vicidial_users' );
+		//$rslt = $this->asteriskDB->query($stmt);
 
-						$i++;
+		if ( $astDB->count < 1 ) {
+			$result 								= 'ERROR';
+			$result_reason 							= "sounds_list USER DOES NOT HAVE PERMISSION TO VIEW SOUNDS LIST";
+			$apiresults 							= array(
+				"result" 								=> "Error".$result_reason
+			);
+		} else {
+			$server_name 							= getenv("SERVER_NAME");
+			//$server_port 							= getenv("SERVER_PORT");
+			$server_port 							= "443";
+			
+			if ( preg_match("443", $server_port) ) {
+				$HTTPprotocol 						= 'https://';
+			} else {
+				$HTTPprotocol 						= 'https://';
+			}
+			
+			$admDIR 								= "$HTTPprotocol$server_name:$server_port";
+			
+			#############################################
+			##### START SYSTEM_SETTINGS LOOKUP #####
+			//$stmtOne = "SELECT use_non_latin,sounds_central_control_active,sounds_web_server,sounds_web_directory FROM system_settings;";
+			$rslt 									= $astDB->getOne('system_settings');
+			
+			if ( $astDB->count > 0 ) {
+				$non_latin 							= $fresults['use_non_latin'];
+				$sounds_central_control_active 		= $fresults['sounds_central_control_active'];
+				$sounds_web_server 					= $fresults['sounds_web_server'];
+				$sounds_web_directory 				= $fresults['sounds_web_directory'];
+			}
+			
+			##### END SETTINGS LOOKUP #####
+			###########################################
+			
+			if ( $sounds_central_control_active < 1 ) {
+				$result 							= 'ERROR';
+				$result_reason 						= "sounds_list CENTRAL SOUND CONTROL IS NOT ACTIVE";
+				$apiresults 						= array(
+					"result" 							=> "Error: ".$result_reason
+				);
+			} else {
+				$i 									= 0;
+				$filename_sort 						= $MT;
+				#$dirpath = "$WeBServeRRooT/$sounds_web_directory";
+				$dirpath 							= "/var/lib/asterisk/sounds";
+				$dh 								= opendir($dirpath);
+		
+				// if ($DB>0) {echo "DEBUG: sounds_list variables - $dirpath|$stage|$format\n";}
+				while ( false !== ($file = readdir($dh)) ) {
+					# Do not list subdirectories
+					if ( (!is_dir("$dirpath/$file")) and (preg_match('/\.(wav|mp3)$/', $file)) ) {
+						if ( (!is_null($search) && strlen($search) > 0) ) {
+							if ( !preg_match("/$search/", $file) )
+								continue;
+						}
+						
+						if ( file_exists("$dirpath/$file") ) {
+							//      'sample_prompt','date',30;
+							$stage 					= "date";
+
+							$file_names[$i] 		= $file;
+							$file_namesPROMPT[$i] 	= preg_replace("/\.wav$|\.gsm$|\.mp3$/","",$file);
+							$file_epoch[$i] 		= filemtime("$dirpath/$file");
+							$file_dates[$i] 		= date ("Y-m-d H:i:s", filemtime("$dirpath/$file"));
+							$file_sizes[$i] 		= filesize("$dirpath/$file");
+							$file_sizesPAD[$i] 		= sprintf("[%020s]\n",filesize("$dirpath/$file"));
+							
+							if ( preg_match('date', $stage) ) {
+								$file_sort[$i] 		= $file_epoch[$i] . "----------" . $i;
+							}
+							
+							if ( preg_match('name', $stage) ) {
+								$file_sort[$i] 		= $file_names[$i] . "----------" . $i;
+							}
+							
+							if ( preg_match('size', $stage) ) {
+								$file_sort[$i] 		= $file_sizesPAD[$i] . "----------" . $i;
+							}
+
+							$i++;
+						}
 					}
 				}
-			}
-			closedir($dh);
-	
-			if (eregi('date',$stage)) {rsort($file_sort);}
-			if (eregi('name',$stage)) {sort($file_sort);}
-			if (eregi('size',$stage)) {rsort($file_sort);}
-	
-			sleep(1);
-	
-			$k=0;
-			$sf=0;
-			while($k < $i) {
-				$file_split = explode('----------',$file_sort[$k]);
-				$m = $file_split[1];
-				$NOWsize = filesize("$dirpath/$file_names[$m]");
-				//if($file_names == $audiofile){
-				$apiresults = array("result" => "success", "file_name" => $file_names, "file_date" => $file_dates, "file_size" => $file_sizes, "file_poch" => $file_epoch);
-				//} else {
-				//	$apiresults = array("result" => "Error: Audiofile doesn't exist.");
-				//}
-				$k++;
-			}
-		}
+				
+				closedir($dh);
+		
+				if ( preg_match('date', $stage) ) { rsort( $file_sort ); }
+				if ( preg_match('name', $stage) ) { sort( $file_sort ); }
+				if ( preg_match('size', $stage) ) { rsort( $file_sort ); }
+		
+				sleep(1);
+		
+				$k									= 0;
+				$sf  								= 0;
+				
+				while ( $k < $i ) {
+					$file_split 					= explode( '----------', $file_sort[$k] );
+					$m 								= $file_split[1];
+					$NOWsize 						= filesize( "$dirpath/$file_names[$m]" );
+					//if($file_names == $audiofile){
+					$apiresults 					= array(
+						"result" 						=> "success", 
+						"file_name" 					=> $file_names, 
+						"file_date" 					=> $file_dates, 
+						"file_size" 					=> $file_sizes, 
+						"file_poch" 					=> $file_epoch
+					);
 
-    }
-}
+					$k++;
+				}
+			}
+
+		}
+	}
 ?>
