f85995b60b28cbd91c66ab26e58fe82e29d8d515
Chris Lomuntad <chris@iknowitworks.org>
2019-02-01 00:52:47 +0800

    Fix for issue #6934

--- goCustomFields/goAddCustomFields.php
+++ goCustomFields/goAddCustomFields.php
@@ -76,119 +76,119 @@
                 $field_sql .= "ALTER TABLE custom_$list_id ADD $field_label ";
             }
             
-            $apiresults = array("result" => "success", "test_result" => $A_field_options);
-            //if ( ($field_type=='SELECT') or ($field_type=='RADIO') ) {
-            //    //$field_options_array = explode("\n",$A_field_options);
-            //    $A_field_options = str_replace('\r\n', '\n', $A_field_options);
-            //    $field_options_array = explode('\n', $A_field_options);
-            //    $field_options_count = count($field_options_array);
-            //    $te=0;
-            //    while ($te < $field_options_count)
-            //        {
-            //        if (preg_match("/,/",$field_options_array[$te]))
-            //            {
-            //            $field_options_value_array = explode(",",$field_options_array[$te]);
-            //            $field_options_ENUM .= "'".str_replace(" ","_",$field_options_value_array[0]."',");
-            //            }
-            //        $te++;
-            //       }
-            //    
-            //    $field_options_ENUM = preg_replace("/.$/",'',$field_options_ENUM);
-            //    $fieldcatch = $field_options_ENUM;
-            //    $field_cost = strlen($field_options_ENUM);
-            //    if ($field_cost < 1) {$field_cost=1;};
-            //    $field_sql .= "ENUM($field_options_ENUM) ";
-            //}
-            // 
-            //
-            //if ( ($field_type=='MULTI') or ($field_type=='CHECKBOX') ){
-            //    $field_options_array = explode("\n",$A_field_options);
-            //    $field_options_count = count($field_options_array);
-            //    $te=0;
-            //    while ($te < $field_options_count)
-            //        {
-            //        if (preg_match("/,/",$field_options_array[$te]))
-            //            {
-            //            $field_options_value_array = explode(",",$field_options_array[$te]);
-            //            $field_options_ENUM .= "'".str_replace(" ","_",$field_options_value_array[0]."',");
-            //            }
-            //        $te++;
-            //       }
-            //    $field_options_ENUM = preg_replace("/.$/",'',$field_options_ENUM);
-            //    $field_cost = strlen($field_options_ENUM);
-            //    if ($field_cost < 1) {$field_cost=1;};
-            //    $field_sql .= "VARCHAR($field_cost) ";
-            //}
-            //
-            //if ($field_type=='TEXT') {
-            //    if ($field_max < 1) {$field_max=1;};
-            //    $field_sql .= "VARCHAR($field_max) ";
-            //}
-            //
-            //if ($field_type=='AREA') {
-            //    $field_sql .= "TEXT ";
-            //    $field_cost = 15;
-            //}
-            //
-            //if ($field_type=='DATE') {
-            //    $field_sql .= "DATE ";
-            //    $field_cost = 10;
-            //}
-            //
-            //if ($field_type=='TIME') {
-            //    $field_sql .= "TIME ";
-            //    $field_cost = 8;
-            //}
-            //
-            //if ( (!empty($field_default) ) and ($field_type!='AREA') and ($field_type!='DATE') and ($field_type!='TIME') ){
-            //    $field_sql .= "DEFAULT '$field_default'";
-            //}
-            //
-            //if ( empty($field_default) ) {
-            //    $field_sql .="";  
-            //}
-            //
-            //if (!$tableCheckResult){
-            //    $field_sql .= ");";
-            //}else{
-            //    $field_sql .= ";";
-            //}
-            //
-            //if ( ($field_type=='DISPLAY') || ($field_type=='SCRIPT') || (preg_match("/\|$field_label\|/",$vicidial_list_fields)) ){
-            //    //do nothing
-            //}else{
-            //    $stmtCUSTOM="$field_sql";
-            //    $rslt = $astDB->rawQuery($stmtCUSTOM);
-            //}
-            //
-            //$data_cf = array(
-            //    'field_label'       => $field_label, 
-            //    'field_name'        => $field_name, 
-            //    'field_description' => $field_description, 
-            //    'field_rank'        => $field_rank, 
-            //    'field_help'        => $field_help, 
-            //    'field_type'        => $field_type, 
-            //    'field_options'     => $field_options, 
-            //    'field_size'        => $field_size, 
-            //    'field_max'         => $field_max, 
-            //    'field_default'     => $field_default, 
-            //    'field_required'    => $field_required, 
-            //    'field_cost'        => $field_cost, 
-            //    'list_id'           => $list_id, 
-            //    'multi_position'    => $multi_position, 
-            //    'name_position'     => $name_position, 
-            //    'field_order'       => $field_order
-            //);
-            //$insertCF = $astDB->insert('vicidial_lists_fields', $data_cf);
-            //$insertQuery = $astDB->getLastQuery();
-            //
-            //if($astDB->getInsertId()){
-            //    $log_id = log_action($goDB, 'ADD', $log_user, $ip_address, "Added a New Custom Field $field_label on List ID $list_id", $log_group, $insertQuery);
-            //   
-            //    $apiresults = array("result" => "success");
-            //}else{
-            //    $apiresults = array("result" => "Error: Failed to add custom field.", "query" => $insertQuery);
-            //}
+            var_dump($A_field_options);die();
+            if ( ($field_type=='SELECT') or ($field_type=='RADIO') ) {
+                //$field_options_array = explode("\n",$A_field_options);
+                $A_field_options = str_replace('\r\n', '\n', $A_field_options);
+                $field_options_array = explode('\n', $A_field_options);
+                $field_options_count = count($field_options_array);
+                $te=0;
+                while ($te < $field_options_count)
+                    {
+                    if (preg_match("/,/",$field_options_array[$te]))
+                        {
+                        $field_options_value_array = explode(",",$field_options_array[$te]);
+                        $field_options_ENUM .= "'".str_replace(" ","_",$field_options_value_array[0]."',");
+                        }
+                    $te++;
+                   }
+                
+                $field_options_ENUM = preg_replace("/.$/",'',$field_options_ENUM);
+                $fieldcatch = $field_options_ENUM;
+                $field_cost = strlen($field_options_ENUM);
+                if ($field_cost < 1) {$field_cost=1;};
+                $field_sql .= "ENUM($field_options_ENUM) ";
+            }
+             
+            
+            if ( ($field_type=='MULTI') or ($field_type=='CHECKBOX') ){
+                $field_options_array = explode("\n",$A_field_options);
+                $field_options_count = count($field_options_array);
+                $te=0;
+                while ($te < $field_options_count)
+                    {
+                    if (preg_match("/,/",$field_options_array[$te]))
+                        {
+                        $field_options_value_array = explode(",",$field_options_array[$te]);
+                        $field_options_ENUM .= "'".str_replace(" ","_",$field_options_value_array[0]."',");
+                        }
+                    $te++;
+                   }
+                $field_options_ENUM = preg_replace("/.$/",'',$field_options_ENUM);
+                $field_cost = strlen($field_options_ENUM);
+                if ($field_cost < 1) {$field_cost=1;};
+                $field_sql .= "VARCHAR($field_cost) ";
+            }
+            
+            if ($field_type=='TEXT') {
+                if ($field_max < 1) {$field_max=1;};
+                $field_sql .= "VARCHAR($field_max) ";
+            }
+            
+            if ($field_type=='AREA') {
+                $field_sql .= "TEXT ";
+                $field_cost = 15;
+            }
+            
+            if ($field_type=='DATE') {
+                $field_sql .= "DATE ";
+                $field_cost = 10;
+            }
+            
+            if ($field_type=='TIME') {
+                $field_sql .= "TIME ";
+                $field_cost = 8;
+            }
+            
+            if ( (!empty($field_default) ) and ($field_type!='AREA') and ($field_type!='DATE') and ($field_type!='TIME') ){
+                $field_sql .= "DEFAULT '$field_default'";
+            }
+            
+            if ( empty($field_default) ) {
+                $field_sql .="";  
+            }
+            
+            if (!$tableCheckResult){
+                $field_sql .= ");";
+            }else{
+                $field_sql .= ";";
+            }
+            
+            if ( ($field_type=='DISPLAY') || ($field_type=='SCRIPT') || (preg_match("/\|$field_label\|/",$vicidial_list_fields)) ){
+                //do nothing
+            }else{
+                $stmtCUSTOM="$field_sql";
+                $rslt = $astDB->rawQuery($stmtCUSTOM);
+            }
+
+            $data_cf = array(
+                'field_label'       => $field_label, 
+                'field_name'        => $field_name, 
+                'field_description' => $field_description, 
+                'field_rank'        => $field_rank, 
+                'field_help'        => $field_help, 
+                'field_type'        => $field_type, 
+                'field_options'     => $field_options, 
+                'field_size'        => $field_size, 
+                'field_max'         => $field_max, 
+                'field_default'     => $field_default, 
+                'field_required'    => $field_required, 
+                'field_cost'        => $field_cost, 
+                'list_id'           => $list_id, 
+                'multi_position'    => $multi_position, 
+                'name_position'     => $name_position, 
+                'field_order'       => $field_order
+            );
+            $insertCF = $astDB->insert('vicidial_lists_fields', $data_cf);
+            $insertQuery = $astDB->getLastQuery();
+            
+            if($astDB->getInsertId()){
+                $log_id = log_action($goDB, 'ADD', $log_user, $ip_address, "Added a New Custom Field $field_label on List ID $list_id", $log_group, $insertQuery);
+               
+                $apiresults = array("result" => "success");
+            }else{
+                $apiresults = array("result" => "Error: Failed to add custom field.", "query" => $insertQuery);
+            }
         }
     }
 ?>
