eab70ff50ed8fc0267791f183616116c99743404
Chris <chris@iknowitworks.org>
2019-05-30 04:23:43 +0800

    Fixing the bug for issue #7321

--- goDispositions/goGetAllDispositions.php
+++ goDispositions/goGetAllDispositions.php
@@ -103,7 +103,7 @@
 					foreach ($result2 as $fresults) {
                         $cCamp                          = $fresults["campaign_id"];
                         $cStatus                        = $fresults["status"];
-                        $custom_dispo[$cStatus][]       = $cCamp;
+                        $custom_dispo[$cCamp][]       = $cStatus;
 					}
 					
 					//GET SYSTEM STATUSES
