38273528dbf5e69b4a68f00eaa3f0df1f5b99d72
Thom <patacsil.thom@gmail.com>
2019-09-10 18:23:04 +0800

    Consolidated standard variables to goAPI.php

--- goScripts/goGetSubscripts.php
+++ goScripts/goGetSubscripts.php
@@ -22,13 +22,7 @@
 */
 
 	include_once ("goAPI.php");	 
- 
-	$log_user 											= $session_user;
-	$log_group 											= go_get_groupid($session_user, $astDB); 
-	$log_ip 											= $astDB->escape($_REQUEST['log_ip']);
-	$goUser												= $astDB->escape($_REQUEST['goUser']);
-	$goPass												= (isset($_REQUEST['log_pass']) ? $astDB->escape($_REQUEST['log_pass']) : $astDB->escape($_REQUEST['goPass']));
-		
+ 	
     // Error Checking
 	if (empty($goUser) || is_null($goUser)) {
 		$apiresults 									= array(
