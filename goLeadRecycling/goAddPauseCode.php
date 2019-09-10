cf58e58bb6b8934c898f3ddd0f932c247fbb1aea
Thom <patacsil.thom@gmail.com>
2019-09-10 12:02:51 +0800

    Consolidated standard variables to goAPI.php

--- goLeadRecycling/goAddPauseCode.php
+++ goLeadRecycling/goAddPauseCode.php
@@ -20,7 +20,8 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+	include_once ("goAPI.php");
+	
     ### POST or GET Variables
 	$camp = $astDB->escape($_REQUEST['pauseCampID']);
 	$pause_code = $astDB->escape($_REQUEST['pause_code']);
