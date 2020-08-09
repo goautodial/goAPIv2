e42cc23f8f10033fb7f945ee86e0e97a54e10812
Ilya <chgpbase@gmail.com>
2020-08-09 22:03:08 +0500

    Fix Extension save bug
    Fix Extension save bug

--- goInbound/goEditIngroup.php
+++ goInbound/goEditIngroup.php
@@ -59,7 +59,8 @@
 	$play_welcome_message 							= $astDB->escape($_REQUEST['play_welcome_message']);
 	$moh_context 									= $astDB->escape($_REQUEST['moh_context']);
 	$onhold_prompt_filename 						= $astDB->escape($_REQUEST['onhold_prompt_filename']);
-	
+	$no_agents_extension							= $astDB->escape($_REQUEST['no_agents_extension']);
+    	$no_agents_extension_context						= $astDB->escape($_REQUEST['no_agents_extension_context']);
 
     // Default values 
     $defActive 										= array("Y","N");
@@ -167,6 +168,9 @@
 				$no_agent_action_value				=$after_hours_callmenu;			
 			}
 			
+			if (!is_null($no_agents_extension) && !is_null($no_agents_extension_context) & $no_agent_action == "EXTENSION") {
+				$no_agent_action_value				=$no_agents_extension.','.$no_agents_extension_context;
+			}
 			$data 									= array(
 				"group_id" 								=> $group_id,
 				"group_name" 							=> $group_name,
