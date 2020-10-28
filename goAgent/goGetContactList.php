41b7973ff35a7a7256d2d7ad1a44f1f49d9e1000
Chris <chris@iknowitworks.org>
2020-10-29 01:44:19 +0800

    Fixing the Agent UI contact list.

--- goAgent/goGetContactList.php
+++ goAgent/goGetContactList.php
@@ -65,6 +65,7 @@ if (count($list_ids) > 0 ) {
     $astDB->where('vl.list_id', $list_ids, 'in');
     $astDB->where('vl.status', array('DNC', 'DNCL'), 'not in');
     $astDB->join('vicidial_lists vls', 'vls.list_id=vl.list_id', 'left');
+    $astDB->orderBy('lead_id');
     $rslt = $astDB->get('vicidial_list vl', $limit, 'lead_id,first_name,middle_initial,last_name,phone_number,last_local_call_time,campaign_id,status,comments,phone_code');
     $lastQuery = $astDB->getLastQuery();
 
