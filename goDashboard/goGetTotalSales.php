5eb16982f252e905e20123406c02e7a0793ad7d9
Nathaniel Cabanban <nathaniel.cabanban24@gmail.com>
2022-08-22 09:38:42 -0400

    Fix on Bug#9088: Dashboard Realtime Sales Monitoring

--- goDashboard/goGetTotalSales.php
+++ goDashboard/goGetTotalSales.php
@@ -51,6 +51,25 @@
 		$userlevel										= $fresults["user_level"];
 		
 		if ($goapiaccess > 0 && $userlevel > 7) {
+            $astDB->where('user_group', $log_group);
+            $allowed_camps = $astDB->getOne('vicidial_user_groups', 'allowed_campaigns');
+            
+            if (strtoupper($log_group) !== 'ADMIN') {
+                $allowed_campaigns = trim($allowed_camps['allowed_campaigns'], "-");
+                if (!preg_match("/ALL-CAMPAIGN/", $allowed_campaigns)) {
+                    $campaigns = explode(" ", trim($allowed_campaigns));
+                }
+
+                //get inbound groups
+                $getIngroups                            = $astDB->where('user_group', $log_group)
+                    ->get('vicidial_inbound_groups', NULL, array('group_id'));
+
+                $ingroups                               = array();
+                foreach ($getIngroups as $fresults) {
+                    $ingroups[]                         = $fresults['group_id'];
+                }
+            }
+
 			if (is_array($campaigns)) {
 				//$status									= array("SALE");
 				$default_status = array("SALE");
@@ -96,21 +115,27 @@
 					
 					case "in-daily":
 					
+                    if (strtoupper($log_group) !== 'ADMIN') {
+                        $astDB->where("vcl.campaign_id", $ingroups, "IN");
+                    }
 					$data 								= $astDB
 						->join("vicidial_list vl", "vcl.lead_id = vl.lead_id", "LEFT")
 						->where("vcl.status", $status, "IN")
 						->where("vcl.call_date", array("$datetoday 00:00:00", "$datetoday 23:59:59"), "BETWEEN")
-						//->where("vcl.campaign_id", $campaigns, "IN")
-						->getValue("vicidial_closer_log  vcl", "count(*)");
+						// ->where("vcl.campaign_id", $ingroups, "IN")
+						->getValue("vicidial_closer_log vcl", "count(*)");
 					break;
 					
 					case "in-hourly":
 					
+                    if (strtoupper($log_group) !== 'ADMIN') {
+                        $astDB->where("vcl.campaign_id", $ingroups, "IN");
+                    }
 					$data 								= $astDB
 						->join("vicidial_list vl", "vcl.lead_id = vl.lead_id", "LEFT")
 						->where("vcl.status", $status, "IN")
 						->where("vcl.call_date", array("$datehourly:00:00", "$datehourly:59:59"), "BETWEEN")
-						//->where("vcl.campaign_id", $campaigns, "IN")
+						// ->where("vcl.campaign_id", $ingroups, "IN")
 						->getValue("vicidial_closer_log  vcl", "count(*)");
 					break;			
 					
@@ -122,12 +147,16 @@
 						->where("vlog.campaign_id", $campaigns, "IN")
 						->getValue("vicidial_log as vlog", "count(*)");
 				
+                    if (strtoupper($log_group) !== 'ADMIN') {
+                        $astDB->where("vcl.campaign_id", $ingroups, "IN");
+                    }
 					$insales = $astDB
 						->join("vicidial_list vl", "vcl.lead_id = vl.lead_id", "LEFT")
 						->where("vcl.status", $status, "IN")
 						->where("vcl.call_date", array("$datetoday 00:00:00", "$datetoday 23:59:59"), "BETWEEN")
-						//->where("vcl.campaign_id", $campaigns, "IN")
+						// ->where("vcl.campaign_id", $ingroups, "IN")
 						->getValue("vicidial_closer_log  vcl", "count(*)");
+                        $test = $astDB->getLastQuery();
 					
 					$data = $insales + $outsales;
 					break;
