eda92c9418f504769789fbc82b23ab5bb912c98f
Nathaniel Cabanban <nathaniel.cabanban24@gmail.com>
2022-08-25 09:26:32 -0400

    Fix on Bug#9079: Carrier Settings

--- goCarriers/goGetAllCarriers.php
+++ goCarriers/goGetAllCarriers.php
@@ -62,7 +62,9 @@
 					if ($userlevel > 8) {
 						$astDB->where("user_group", $log_group);
 						$astDB->orWhere("user_group", "---ALL---");
-					}
+					} else {
+						$astDB->where("user_group", $log_group);
+                    }
 				}					
 			}
 
